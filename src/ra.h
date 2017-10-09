be45e214570d2717cbc856273b8efd976a2c5430
Niklas Haas <git@haasn.xyz>
2017-10-09 08:48:01 +0200

    ra: reduce amount of boilerplate for ra_fns

--- src/ra.h
+++ src/ra.h
@@ -19,54 +19,41 @@
 
 #include "common.h"
 
-// Note: The `destroy` functions also free the corresponding objects, but
-// must not be called on NULL. (The NULL checks are done by the ra_*_destroy
-// wrappers)
+#define RA_PFN(name) __typeof__(ra_##name) *name
+
 struct ra_fns {
+    // Destructors: These also free the corresponding objects, but they
+    // must not be called on NULL. (The NULL checks are done by the ra_*_destroy
+    // wrappers)
     void (*destroy)(const struct ra *ra);
+    void (*tex_destroy)(const struct ra *, const struct ra_tex *);
+    void (*buf_destroy)(const struct ra *, const struct ra_buf *);
+    void (*renderpass_destroy)(const struct ra *, const struct ra_renderpass *);
 
-    const struct ra_tex *(*tex_create)(const struct ra *ra,
-                                       const struct ra_tex_params *params);
-    void (*tex_destroy)(const struct ra *ra, const struct ra_tex *tex);
-    void (*tex_clear)(const struct ra *ra, const struct ra_tex *dst,
-                      struct pl_rect3d rect, const float color[4]);
-    // Optional if RA_CAP_TEX_BLIT is not present.
-    void (*tex_blit)(const struct ra *ra,
-                 const struct ra_tex *dst, const struct ra_tex *src,
-                 struct pl_rect3d dst_rc, struct pl_rect3d src_rc);
-    bool (*tex_upload)(const struct ra *ra,
-                       const struct ra_tex_upload_params *params);
-
-    const struct ra_buf *(*buf_create)(const struct ra *ra,
-                                       const struct ra_buf_params *params);
-    void (*buf_destroy)(const struct ra *ra, const struct ra_buf *buf);
-    void (*buf_update)(const struct ra *ra, const struct ra_buf *buf,
-                       size_t buf_offset, const void *data, size_t size);
-    // Optional, if NULL it's assumed that ra_buf_poll always returns true.
-    bool (*buf_poll)(const struct ra *ra, const struct ra_buf *buf);
-
-    // The following functions are optional if the corresponding ra_limit
-    // size restriction is 0
-    struct ra_var_layout (*buf_uniform_layout)(const struct ra *, size_t offset,
-                                               const struct ra_var *var);
-    struct ra_var_layout (*buf_storage_layout)(const struct ra *ra, size_t offset,
-                                               const struct ra_var *var);
-    struct ra_var_layout (*push_constant_layout)(const struct ra *ra, size_t offset,
-                                                 const struct ra_var *var);
-
-    int (*desc_namespace)(const struct ra *ra, enum ra_desc_type type);
-    const struct ra_renderpass *(*renderpass_create)(const struct ra *ra,
-                                    const struct ra_renderpass_params *params);
-    void (*renderpass_destroy)(const struct ra *ra,
-                               const struct ra_renderpass *pass);
-    void (*renderpass_run)(const struct ra *ra,
-                           const struct ra_renderpass_run_params *params);
+    RA_PFN(tex_create);
+    RA_PFN(tex_clear);
+    RA_PFN(tex_blit); // optional if RA_CAP_TEX_BLIT is not present
+    RA_PFN(tex_upload);
+    RA_PFN(buf_create);
+    RA_PFN(buf_update);
+    RA_PFN(buf_poll); // optional: if NULL buffers are always free to use
+    RA_PFN(desc_namespace);
+    RA_PFN(renderpass_create);
+    RA_PFN(renderpass_run);
 
     // The following functions are all optional, but they must either all be
     // supported or all be absent. They will never be called on NULL timers
     // (the ra_timer_* wrappers check for this).
-    struct ra_timer *(*timer_create)(const struct ra *ra);
-    void (*timer_destroy)(const struct ra *ra, struct ra_timer *timer);
-    void (*timer_start)(const struct ra *ra, struct ra_timer *timer);
-    uint64_t (*timer_stop)(const struct ra *ra, struct ra_timer *timer);
+    RA_PFN(timer_create);
+    RA_PFN(timer_start);
+    RA_PFN(timer_stop);
+    void (*timer_destroy)(const struct ra *, struct ra_timer *);
+
+    // The following functions are optional if the corresponding ra_limit
+    // size restriction is 0
+    RA_PFN(buf_uniform_layout);
+    RA_PFN(buf_storage_layout);
+    RA_PFN(push_constant_layout);
 };
+
+#undef RA_PFN
