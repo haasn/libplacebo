384e83251cc15118c83b723752c22da6bbf615d2
Niklas Haas <git@haasn.xyz>
2018-01-18 10:19:43 +0100

    ta: increase size of allocation names
    Slightly useless otherwise when the path is included.

--- src/ta/ta.c
+++ src/ta/ta.c
@@ -377,7 +377,7 @@ void ta_print_leak_report(void)
             // Don't list those with parent; logically, only parents are listed
             if (!cur->next) {
                 size_t c_size = get_children_size(cur);
-                char name[30] = {0};
+                char name[256] = {0};
                 if (cur->name)
                     snprintf(name, sizeof(name), "%s", cur->name);
                 if (cur->name == &allocation_is_string) {
