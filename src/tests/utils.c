de715031efe61d9399b8ad8468364e6798c5a11f
Niklas Haas <git@haasn.xyz>
2018-09-29 02:59:29 +0200

    tests: add test cases for std140/std430 packing
    To avoid introducing regressions in the future, now that I'm sure it
    works as it's supposed to.

--- src/tests/utils.c
+++ src/tests/utils.c
@@ -1,4 +1,5 @@
 #include "tests.h"
+#include "gpu.h"
 
 int main()
 {
@@ -25,4 +26,63 @@ int main()
         REQUIRE(data.component_size[i] == 10);
         REQUIRE(data.component_map[i] == i);
     }
+
+    // Test GLSL structure packing
+    struct pl_var vec1 = pl_var_float(""),
+                  vec2 = pl_var_vec2(""),
+                  vec3 = pl_var_vec3(""),
+                  mat2 = pl_var_mat2(""),
+                  mat3 = pl_var_mat3("");
+
+    struct pl_var_layout layout;
+    layout = std140_layout(NULL, 0, &vec2);
+    REQUIRE(layout.offset == 0);
+    REQUIRE(layout.stride == 2 * sizeof(float));
+    REQUIRE(layout.size == 2 * sizeof(float));
+
+    layout = std140_layout(NULL, 3 * sizeof(float), &vec3);
+    REQUIRE(layout.offset == 4 * sizeof(float));
+    REQUIRE(layout.stride == 3 * sizeof(float));
+    REQUIRE(layout.size == 3 * sizeof(float));
+
+    layout = std140_layout(NULL, 2 * sizeof(float), &mat3);
+    REQUIRE(layout.offset == 4 * sizeof(float));
+    REQUIRE(layout.stride == 4 * sizeof(float));
+    REQUIRE(layout.size == 3 * 4 * sizeof(float));
+
+    layout = std430_layout(NULL, 2 * sizeof(float), &mat3);
+    REQUIRE(layout.offset == 4 * sizeof(float));
+    REQUIRE(layout.stride == 3 * sizeof(float));
+    REQUIRE(layout.size == 3 * 3 * sizeof(float));
+
+    layout = std140_layout(NULL, 3 * sizeof(float), &vec1);
+    REQUIRE(layout.offset == 3 * sizeof(float));
+    REQUIRE(layout.stride == sizeof(float));
+    REQUIRE(layout.size == sizeof(float));
+
+    struct pl_var vec2a = vec2;
+    vec2a.dim_a = 50;
+
+    layout = std140_layout(NULL, sizeof(float), &vec2a);
+    REQUIRE(layout.offset == 4 * sizeof(float));
+    REQUIRE(layout.stride == 4 * sizeof(float));
+    REQUIRE(layout.size == 50 * 4 * sizeof(float));
+
+    layout = std430_layout(NULL, sizeof(float), &vec2a);
+    REQUIRE(layout.offset == 2 * sizeof(float));
+    REQUIRE(layout.stride == 2 * sizeof(float));
+    REQUIRE(layout.size == 50 * 2 * sizeof(float));
+
+    struct pl_var mat2a = mat2;
+    mat2a.dim_a = 20;
+
+    layout = std140_layout(NULL, 5 * sizeof(float), &mat2a);
+    REQUIRE(layout.offset == 8 * sizeof(float));
+    REQUIRE(layout.stride == 4 * sizeof(float));
+    REQUIRE(layout.size == 20 * 2 * 4 * sizeof(float));
+
+    layout = std430_layout(NULL, 5 * sizeof(float), &mat2a);
+    REQUIRE(layout.offset == 6 * sizeof(float));
+    REQUIRE(layout.stride == 2 * sizeof(float));
+    REQUIRE(layout.size == 20 * 2 * 2 * sizeof(float));
 }
