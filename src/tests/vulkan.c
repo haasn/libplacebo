ef2db8cbed56b6582e4a2dce789349ef98777c2b
Niklas Haas <git@haasn.xyz>
2018-11-06 09:39:00 +0100

    tests: simplify vulkan test API usage
    With the new instance_params field, we no longer need to separate the
    `pl_vk_inst`.

--- src/tests/vulkan.c
+++ src/tests/vulkan.c
@@ -20,14 +20,9 @@ static void vulkan_tests(const struct pl_gpu *gpu)
 int main()
 {
     struct pl_context *ctx = pl_test_context();
-    struct pl_vk_inst_params iparams = pl_vk_inst_default_params;
-    iparams.debug = true;
-    const struct pl_vk_inst *vkinst = pl_vk_inst_create(ctx, &iparams);
-    if (!vkinst)
-        return SKIP;
 
     struct pl_vulkan_params params = pl_vulkan_default_params;
-    params.instance = vkinst->instance;
+    params.instance_params = &(struct pl_vk_inst_params) { .debug = true };
     const struct pl_vulkan *vk = pl_vulkan_create(ctx, &params);
     if (!vk)
         return SKIP;
@@ -35,6 +30,5 @@ int main()
     vulkan_tests(vk->gpu);
     gpu_tests(vk->gpu);
     pl_vulkan_destroy(&vk);
-    pl_vk_inst_destroy(&vkinst);
     pl_context_destroy(&ctx);
 }
