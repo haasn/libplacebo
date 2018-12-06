ec190ee7d2e28197e9df1d6cdeb3a4403f5d8c14
Niklas Haas <git@haasn.xyz>
2018-12-06 07:08:52 +0100

    malloc: determine the set of supported handles by trial
    Just try asking the driver if it would be okay with a TRANSFER_DST
    buffer for this handle type. If tha fails, so should certainly
    everything else.
    
    Unfortunately, we have no good way of testing which handle types are
    supported for images. We could also use trials here, but we don't really
    distinguish between images and buffers at all in the pl_gpu.handle_caps.
    
    Hopefully this should normally either be supported for both buffers and
    images, or for neither. It's probably best the user just relies on
    `pl_tex_create` failing in environments where buffers are supported but
    images are not.

--- src/vulkan/malloc.c
+++ src/vulkan/malloc.c
@@ -399,14 +399,25 @@ pl_handle_caps vk_malloc_handle_caps(struct vk_malloc *ma)
     struct vk_ctx *vk = ma->vk;
     pl_handle_caps ret = 0;
 
+    static const pl_handle_caps cap_list[] = {
 #ifdef VK_HAVE_UNIX
-    if (vk->vkGetMemoryFdKHR)
-        ret |= PL_HANDLE_FD;
+        PL_HANDLE_FD,
 #endif
 #ifdef VK_HAVE_WIN32
-    if (vk->vkGetMemoryWin32HandleKHR)
-        ret |= (PL_HANDLE_WIN32 | PL_HANDLE_WIN32_KMT);
+        PL_HANDLE_WIN32,
+        PL_HANDLE_WIN32_KMT,
 #endif
+    };
+
+    for (int i = 0; i < PL_ARRAY_SIZE(cap_list); i++) {
+        // Try seeing if we could allocate a "basic" buffer using these
+        // capabilities, with no fancy buffer usage. More specific checks will
+        // happen down the line at VkBuffer creation time, but this should give
+        // us a rough idea of what the driver supports.
+        enum pl_handle_type cap = cap_list[i];
+        if (buf_export_check(vk, VK_BUFFER_USAGE_TRANSFER_DST_BIT, cap))
+            ret |= cap;
+    }
 
     return ret;
 }
