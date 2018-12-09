74aa78807a5c3241d4838a5da91e9dda64488d55
BtbN <btbn@btbn.de>
2018-12-09 16:48:03 +0100

    vulkan: define VK_USE_PLATFORM_WIN32_KHR on Windows

--- src/include/libplacebo/vulkan.h
+++ src/include/libplacebo/vulkan.h
@@ -23,6 +23,10 @@
 #error Included <libplacebo/vulkan.h> but libplacebo built without vulkan!
 #endif
 
+#ifdef _WIN32
+#define VK_USE_PLATFORM_WIN32_KHR
+#endif
+
 #include <vulkan/vulkan.h>
 #include <libplacebo/gpu.h>
 #include <libplacebo/swapchain.h>
