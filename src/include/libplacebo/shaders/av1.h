2ee4b041298c951db49f47a0c00f6d49dc4f3c0b
Niklas Haas <git@haasn.xyz>
2019-03-07 04:53:23 +0100

    shaders/av1: release under MIT
    dav1d people want this to be permissive so they can adapt it for
    documentation purposes. I have no problems releasing this specific file
    under a permissive license.
    
    Note that since both the header and source code heavily depend on the
    LGPLv2.1+ parts of libplacebo, this licensing change isn't terribly
    useful unless you're just interested in copying fragments of the glsl
    code or some of the C helper functions.

--- src/include/libplacebo/shaders/av1.h
+++ src/include/libplacebo/shaders/av1.h
@@ -1,18 +1,27 @@
 /*
- * This file is part of libplacebo.
+ * This file is part of libplacebo, which is normally licensed under the terms
+ * of the LGPL v2.1+. However, this file (av1.h) is also available under the
+ * terms of the more permissive MIT license:
  *
- * libplacebo is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Copyright (c) 2018-2019 Niklas Haas
  *
- * libplacebo is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with libplacebo.  If not, see <http://www.gnu.org/licenses/>.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 #ifndef LIBPLACEBO_SHADERS_AV1_H_
