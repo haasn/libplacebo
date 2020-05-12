f95d0600789fa3038a8ba70f54fcfcc24af0b584
Niklas Haas <git@haasn.xyz>
2020-05-12 06:22:51 +0200

    Revert "filters: add proper nearest neighbour filter"
    This reverts commit b0e6d362b16b2c6930479b7bda818d8c5d4883f3.
    
    This commit made the oversight of not all downsampling kernels being
    resizeable, and in particular, it being resizeable or not has nothing to
    do with our ability to stretch the filter scale. This commit effectively
    broke anti-aliased downsampling for e.g. bcspline-family filters, such
    as the default mitchell filter, which is fairly significant.
    
    I think the only proper way to get nearest neighbour downsampling is to
    set box as the kernel *and* also disable anti-aliasing.

--- src/tests/filters.c
+++ src/tests/filters.c
@@ -35,35 +35,5 @@ int main()
 
         pl_filter_free(&flt);
     }
-
-    // Test nearest neighbour at a variety of scales
-    int size = 1280;
-    for (int offset = 0; offset < 128; offset++) {
-        float scale = (float) size / (size - offset);
-        printf("Testing nearest filter at scale %f\n", scale);
-
-        const struct pl_filter *flt = pl_filter_generate(ctx, &(struct pl_filter_params) {
-            .config = pl_filter_nearest,
-            .lut_entries = 64,
-            .filter_scale = scale,
-        });
-
-        REQUIRE(flt);
-        for (int i = 0; i < 64; i++) {
-            REQUIRE(flt->row_size == 2);
-            const float *w = &flt->weights[i * flt->row_stride];
-
-            if (i < 64 / 2) {
-                REQUIRE(feq(w[0], 1.0, 1e-7));
-                REQUIRE(feq(w[1], 0.0, 1e-7));
-            } else {
-                REQUIRE(feq(w[0], 0.0, 1e-7));
-                REQUIRE(feq(w[1], 1.0, 1e-7));
-            }
-        }
-
-        pl_filter_free(&flt);
-    }
-
     pl_context_destroy(&ctx);
 }
