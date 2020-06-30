haasn/libplacebo

commit f8c8362502af5bb2a1fa05787a54a01562951d58

vulkan: allow host-mapping imported memory
There's nothing technically preventing API users from requesting both
`import_handle` and `host_mapped` at the same time. Additionally, we
benefit internally from using host-mapped memory where such is
available.

For both reasons, try host-mapping all imported memory wherever the
memory heap allows it.

src/vulkan/malloc.c
@@ -633,6 +633,7 @@ bool vk_malloc_import(struct vk_malloc *ma, VkMemoryRequirements reqs,
                       struct vk_memslice *out)
 {
     struct vk_ctx *vk = ma->vk;
+    struct vk_slab *slab = NULL;
 
     if (reqs.size > shared_mem->size) {
         PL_ERR(vk, "Imported object requires memory larger than the provided "
@@ -701,7 +702,7 @@ bool vk_malloc_import(struct vk_malloc *ma, VkMemoryRequirements reqs,
     VK(vk->AllocateMemory(vk->dev, &ainfo, VK_ALLOC, &vkmem));
     // fd ownership is transferred at this point.
 
-    struct vk_slab *slab = talloc_ptrtype(NULL, slab);
+    slab = talloc_ptrtype(NULL, slab);
     *slab = (struct vk_slab) {
         .mem = vkmem,
         .dedicated = true,
@@ -713,6 +714,14 @@ bool vk_malloc_import(struct vk_malloc *ma, VkMemoryRequirements reqs,
         },
         .handle_type = handle_type,
     };
+
+    int heapIndex = ma->props.memoryTypes[ainfo.memoryTypeIndex].heapIndex;
+    const VkMemoryHeap *heap = &ma->props.memoryHeaps[heapIndex];
+    if (heap->flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+        VK(vk->MapMemory(vk->dev, slab->mem, 0, VK_WHOLE_SIZE, 0, &slab->data));
+        slab->coherent = heap->flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+    }
+
     *out = (struct vk_memslice) {
         .vkmem = vkmem,
         .size = shared_mem->size,
@@ -729,6 +738,7 @@ bool vk_malloc_import(struct vk_malloc *ma, VkMemoryRequirements reqs,
 error:
     if (fd > -1)
         close(fd);
+    talloc_free(slab);
     return false;
 
 #endif // VK_HAVE_UNIX

src/vulkan/gpu.c
@@ -1847,6 +1847,12 @@ static const struct pl_buf *vk_buf_create(const struct pl_gpu *gpu,
                               &params->shared_mem, &buf_vk->slice.mem))
             goto error;
 
+        if (params->host_mapped && !buf_vk->slice.mem.data) {
+            PL_ERR(gpu, "Failed creating host-mapped buffer from host-invisible "
+                   "imported memory!");
+            goto error;
+        }
+
         buf_vk->slice.buf = buf_vk->import_buf;
         VK(vk->BindBufferMemory(vk->dev, buf_vk->import_buf,
                                 buf_vk->slice.mem.vkmem, 0));

commit db5a1fd3a28379ff06c73d460fd3b837284b40fa

vulkan: fix memory type property check when importing
Commit f8c8362502af introduced this logic but compared against the wrong
set of flags. More importantly, it also forgot to forward the
data/coherent attributes to the actual `vk_memslice`!

src/vulkan/malloc.c
@@ -715,13 +715,6 @@ bool vk_malloc_import(struct vk_malloc *ma, VkMemoryRequirements reqs,
         .handle_type = handle_type,
     };
 
-    int heapIndex = ma->props.memoryTypes[ainfo.memoryTypeIndex].heapIndex;
-    const VkMemoryHeap *heap = &ma->props.memoryHeaps[heapIndex];
-    if (heap->flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
-        VK(vk->MapMemory(vk->dev, slab->mem, 0, VK_WHOLE_SIZE, 0, &slab->data));
-        slab->coherent = heap->flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
-    }
-
     *out = (struct vk_memslice) {
         .vkmem = vkmem,
         .size = shared_mem->size,
@@ -729,6 +722,14 @@ bool vk_malloc_import(struct vk_malloc *ma, VkMemoryRequirements reqs,
         .shared_mem = *shared_mem,
         .priv = slab,
     };
+
+    VkMemoryPropertyFlags flags = ma->props.memoryTypes[ainfo.memoryTypeIndex].propertyFlags;
+    if (flags & VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT) {
+        VK(vk->MapMemory(vk->dev, slab->mem, 0, VK_WHOLE_SIZE, 0, &slab->data));
+        slab->coherent = flags & VK_MEMORY_PROPERTY_HOST_COHERENT_BIT;
+        out->data = (uint8_t *) slab->data + out->offset;
+        out->coherent = slab->coherent;
+    }
 
     PL_DEBUG(vk, "Importing %zu of memory from fd: %d",
              (size_t) slab->size, fd);