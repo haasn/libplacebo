18845b128d2ecd578b81717524c0ac8dd74cf9fa
Niklas Haas <git@haasn.dev>
2021-07-27 14:21:33 +0200

    dispatch: fix format equality test
    This actually needs to test for the full format (pointer) equality, not
    just the name. This fixes an issue where FBO format changes could
    trigger validation errors.

--- src/dispatch.c
+++ src/dispatch.c
@@ -767,7 +767,7 @@ static struct pass *finalize_pass(pl_dispatch dp, pl_shader sh,
         pl_hash_merge(&pass->signature, (uint64_t) params.vertex_type);
         pl_hash_merge(&pass->signature, (uint64_t) params.vertex_stride);
         pl_hash_merge(&pass->signature, (uint64_t) params.load_target);
-        pl_hash_merge(&pass->signature, pl_str0_hash(target->params.format->name));
+        pl_hash_merge(&pass->signature, (uint64_t) target->params.format); // hash ptr
         if (blend)
             pl_hash_merge(&pass->signature, pl_mem_hash(blend, sizeof(*blend)));
     }
