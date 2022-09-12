63c30d52f4b52b117a3210e0c0ac1ebb7ff84a9e
Niklas Haas <git@haasn.dev>
2022-09-13 00:10:39 +0200

    shaders/icc: switch to tetrahedral interpolation

--- src/shaders/icc.c
+++ src/shaders/icc.c
@@ -502,7 +502,7 @@ void pl_icc_decode(pl_shader sh, pl_icc_object icc, pl_shader_obj *lut_obj,
     ident_t lut = sh_lut(sh, sh_lut_params(
         .object     = lut_obj,
         .var_type   = PL_VAR_FLOAT,
-        .method     = SH_LUT_LINEAR,
+        .method     = SH_LUT_TETRAHEDRAL,
         .fmt        = pl_find_fmt(SH_GPU(sh), PL_FMT_UNORM, 4,
                                   16, 16, PL_FMT_CAP_LINEAR),
         .width      = icc->params.size_r,
@@ -546,7 +546,7 @@ void pl_icc_encode(pl_shader sh, pl_icc_object icc, pl_shader_obj *lut_obj)
     ident_t lut = sh_lut(sh, sh_lut_params(
         .object     = lut_obj,
         .var_type   = PL_VAR_FLOAT,
-        .method     = SH_LUT_LINEAR,
+        .method     = SH_LUT_TETRAHEDRAL,
         .fmt        = pl_find_fmt(SH_GPU(sh), PL_FMT_UNORM, 4,
                                   16, 16, PL_FMT_CAP_LINEAR),
         .width      = icc->params.size_r,
