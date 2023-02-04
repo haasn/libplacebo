1f1d708e22c7db579b0190aa5292bbc45bd9dfaf
Niklas Haas <git@haasn.dev>
2023-02-04 18:21:56 +0100

    tone_mapping: default to ST2094 when metadata present
    This generally produces good results in such a case. ST2094-40 is better
    than ST2094-10 even in the absence of metadata, because the relevant
    parts of ST2094-10 (the average->average mapping) are also implemented
    in the ST2094-40 fallback path, and more importantly, the latter also
    implements proper black point adaptation (to avoid black crush).
    
    In the future, ST2094-40 (possibly with some tweaks) might become the
    one-size-fits-all default tone-mapping method for all scenarios.

--- src/tone_mapping.c
+++ src/tone_mapping.c
@@ -116,7 +116,10 @@ void pl_tone_map_params_infer(struct pl_tone_map_params *par)
         float src_max = pl_hdr_rescale(par->input_scaling, PL_HDR_NORM, par->input_max);
         float dst_max = pl_hdr_rescale(par->output_scaling, PL_HDR_NORM, par->output_max);
         float ratio = src_max / dst_max;
-        if (ratio > 10) {
+        if (par->hdr.ootf.num_anchors || par->hdr.scene_avg) {
+            // HDR10+ metadata available: Pick SMPTE ST2094-40
+            par->function = &pl_tone_map_st2094_40;
+        } else if (ratio > 10) {
             // Extreme reduction: Pick spline for its quasi-linear behavior
             par->function = &pl_tone_map_spline;
         } else if (src_max < 1 + 1e-3 && dst_max < 1 + 1e-3) {
