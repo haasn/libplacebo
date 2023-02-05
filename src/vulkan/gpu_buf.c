haasn/libplacebo

commit 8a8d84564c69a6963b4e0aa39cf2ec39abceee52

vulkan/buf: relax HOST_CACHED requirement for buffers
From `required` to `optimal`, since `pl_gpu_limits.host_cached` can be
used to test for the presence of host-cached buffers to avoid depending
on slow paths here.

src/vulkan/gpu_buf.c
@@ -170,9 +170,9 @@ pl_buf vk_buf_create(pl_gpu gpu, const struct pl_buf_params *params)
         mparams.required |= VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
 
         if (params->size > 1024) {
-            // Require cached memory for large buffers (1 kB) which may be read
+            // Prefer cached memory for large buffers (1 kB) which may be read
             // from, because uncached reads are extremely slow
-            mparams.required |= VK_MEMORY_PROPERTY_HOST_CACHED_BIT;
+            mparams.optimal |= VK_MEMORY_PROPERTY_HOST_CACHED_BIT;
         }
     }
 

commit 36ed28fc340983b4c4d4f2e0dea0d4619affd266

vulkan/buf: refactor memory selection logic
Make a number of changes to this logic in the hopes of picking stupid
combinations (like device-local host-cached memory, or host-local shader
storage buffers) less often.

Fixes: 8a8d8456
Fixes: https://github.com/mpv-player/mpv/issues/11270

src/vulkan/gpu_buf.c
@@ -176,17 +176,13 @@ pl_buf vk_buf_create(pl_gpu gpu, const struct pl_buf_params *params)
         }
     }
 
-    if (params->host_writable || params->host_readable) {
-        // Prefer buffers requiring frequent host operations in host mem
-        mem_type = PL_DEF(mem_type, PL_BUF_MEM_HOST);
-    }
-
     switch (mem_type) {
     case PL_BUF_MEM_AUTO:
         // We generally prefer VRAM since it's faster than RAM, but any number
         // of other requirements could potentially exclude it, so just mark it
         // as optimal by default.
-        mparams.optimal |= VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
+        if (!(mparams.optimal & VK_MEMORY_PROPERTY_HOST_CACHED_BIT))
+            mparams.optimal |= VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT;
         break;
     case PL_BUF_MEM_DEVICE:
         // Force device local memory.
@@ -197,6 +193,7 @@ pl_buf vk_buf_create(pl_gpu gpu, const struct pl_buf_params *params)
         // device-local bit locks out all memory heaps on iGPUs. Requiring
         // the memory be host-mapped is the easiest compromise.
         mparams.required |= VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT;
+        mparams.optimal  |= VK_MEMORY_PROPERTY_HOST_CACHED_BIT;
         break;
     case PL_BUF_MEM_TYPE_COUNT:
         pl_unreachable();