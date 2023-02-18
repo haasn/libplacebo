haasn/libplacebo

commit 82023b9e9d683499329b2d5c502eafaf5f6c8ef2

common: move GCC visibility pragmas to PL_API_BEGIN/END
Allows us to avoid having to include every header file here.

src/common.h
@@ -46,13 +46,16 @@
 #include "pl_alloc.h"
 #include "pl_string.h"
 
-// Include all of the symbols that should be public in a way that marks them
-// as being externally visible. (Otherwise, all symbols are hidden by default)
-#pragma GCC visibility push(default)
-
 #include <libplacebo/config.h>
 #undef PL_DEPRECATED
+#undef PL_API_BEGIN
+#undef PL_API_END
 #define PL_DEPRECATED
+
+// Include all of the symbols that should be public in a way that marks them
+// as being externally visible. (Otherwise, all symbols are hidden by default)
+#define PL_API_BEGIN _Pragma("GCC visibility push(default)")
+#define PL_API_END _Pragma("GCC visibility pop")
 
 #if PL_API_VER != BUILD_API_VER
 #error Header mismatch? <libplacebo/config.h> pulled from elsewhere!
@@ -85,8 +88,6 @@
 #ifdef PL_HAVE_WIN32
 #include <libplacebo/d3d11.h>
 #endif
-
-#pragma GCC visibility pop
 
 // Divide a number while rounding up (careful: double-eval)
 #define PL_DIV_UP(x, y) (((x) + (y) - 1) / (y))

commit c7854aac2625ebe42972e85e4a1c851cdac5ae95

config: fix config.h API definitions
This needs to be properly defined as well even after removal of the
pragma before global includes.

Fixes: 82023b9e9d683499329b2d5c502eafaf5f6c8ef2

src/common.h
@@ -46,16 +46,14 @@
 #include "pl_alloc.h"
 #include "pl_string.h"
 
-#include <libplacebo/config.h>
-#undef PL_DEPRECATED
-#undef PL_API_BEGIN
-#undef PL_API_END
 #define PL_DEPRECATED
 
 // Include all of the symbols that should be public in a way that marks them
 // as being externally visible. (Otherwise, all symbols are hidden by default)
 #define PL_API_BEGIN _Pragma("GCC visibility push(default)")
 #define PL_API_END _Pragma("GCC visibility pop")
+
+#include <libplacebo/config.h>
 
 #if PL_API_VER != BUILD_API_VER
 #error Header mismatch? <libplacebo/config.h> pulled from elsewhere!

src/include/libplacebo/config.h.in
@@ -31,38 +31,44 @@
 // Friendly name (`git describe`) for the overall version of the library
 #define PL_VERSION (pl_version())
 
-int pl_fix_ver(void);
-const char *pl_version(void);
-
 // Feature tests. These aren't described in further detail, but may be useful
 // for programmers wanting to programmatically check for feature support
 // in their compiled libplacebo versions.
 @extra_defs@
 
 // Extra compiler-specific stuff
-#if defined(_MSC_VER)
-#define PL_DEPRECATED
-#else
-#define PL_DEPRECATED __attribute__((deprecated))
+#ifndef PL_DEPRECATED
+# if defined(_MSC_VER)
+# define PL_DEPRECATED
+# else
+# define PL_DEPRECATED __attribute__((deprecated))
+# endif
 #endif
 
 #ifndef __has_feature
 #define __has_feature(x) 0
 #endif
 
-#if (defined(__GNUC__) && (__GNUC__ >= 6)) || __has_feature(enumerator_attributes)
-#define PL_DEPRECATED_ENUMERATOR PL_DEPRECATED
-#else
-#define PL_DEPRECATED_ENUMERATOR
+#ifndef PL_DEPRECATED_ENUMERATOR
+# if (defined(__GNUC__) && (__GNUC__ >= 6)) || __has_feature(enumerator_attributes)
+# define PL_DEPRECATED_ENUMERATOR PL_DEPRECATED
+# else
+# define PL_DEPRECATED_ENUMERATOR
+# endif
 #endif
 
 // C++ compatibility
-#ifdef __cplusplus
-# define PL_API_BEGIN extern "C" {
-# define PL_API_END }
-#else
-# define PL_API_BEGIN
-# define PL_API_END
+#ifndef PL_API_BEGIN
+# ifdef __cplusplus
+#  define PL_API_BEGIN extern "C" {
+#  define PL_API_END }
+# else
+#  define PL_API_BEGIN
+#  define PL_API_END
+# endif
+#endif
+
+#ifndef __cplusplus
 // Disable this warning because libplacebo's params macros override fields
 # pragma GCC diagnostic ignored "-Woverride-init"
 #endif
@@ -74,4 +80,11 @@ const char *pl_version(void);
 // Deprecated macro for back-compatibility
 #define PL_STRUCT(name) struct name##_t
 
-#endif // LIBPLACEBO_CONTEXT_H_
+PL_API_BEGIN
+
+int pl_fix_ver(void);
+const char *pl_version(void);
+
+PL_API_END
+
+#endif // LIBPLACEBO_CONFIG_H_