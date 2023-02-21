4e9596835bf04410028d583480820d21b0da4ad1
Kacper Michajłow <kasper93@gmail.com>
2023-02-21 20:03:47 +0100

    common: add nonnull attribute PL_PRINTF
    To catch more incorrect substitutions errors.

--- src/common.h
+++ src/common.h
@@ -26,9 +26,11 @@
 #include <inttypes.h>
 
 #if defined(__MINGW32__) && !defined(__clang__)
-#define PL_PRINTF(fmt, va) __attribute__ ((format(gnu_printf, fmt, va)))
+#define PL_PRINTF(fmt, va) __attribute__ ((format(gnu_printf, fmt, va))) \
+                           __attribute__ ((nonnull))
 #elif defined(__GNUC__)
-#define PL_PRINTF(fmt, va) __attribute__ ((format(printf, fmt, va)))
+#define PL_PRINTF(fmt, va) __attribute__ ((format(printf, fmt, va))) \
+                           __attribute__ ((nonnull))
 #else
 #define PL_PRINTF(fmt, va)
 #endif
