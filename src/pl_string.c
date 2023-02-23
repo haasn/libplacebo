4fb6a7b2912d4cbf8dbe567aa9a9b2cb8153800c
Niklas Haas <git@haasn.dev>
2023-02-23 19:57:40 +0100

    pl_string: add pl_str_builder
    Will replace `pl_str` inside `pl_shader`, with the ultimate goal of
    making shader construction more efficient.

--- src/pl_string.c
+++ src/pl_string.c
@@ -231,3 +231,171 @@ error:
     pl_free(buf);
     return false;
 }
+
+struct pl_str_builder_t {
+    PL_ARRAY(pl_str_template) templates;
+    pl_str args;
+    pl_str output;
+};
+
+pl_str_builder pl_str_builder_alloc(void *alloc)
+{
+    pl_str_builder b = pl_zalloc_ptr(alloc, b);
+    return b;
+}
+
+void pl_str_builder_free(pl_str_builder *b)
+{
+    if (*b)
+        pl_free_ptr(b);
+}
+
+void pl_str_builder_reset(pl_str_builder b)
+{
+    *b = (struct pl_str_builder_t) {
+        .templates.elem = b->templates.elem,
+        .args.buf       = b->args.buf,
+        .output.buf     = b->output.buf,
+    };
+}
+
+uint64_t pl_str_builder_hash(const pl_str_builder b)
+{
+    size_t size = b->templates.num * sizeof(b->templates.elem[0]);
+    uint64_t hash = pl_mem_hash(b->templates.elem, size);
+    pl_hash_merge(&hash, pl_str_hash(b->args));
+    return hash;
+}
+
+pl_str pl_str_builder_exec(pl_str_builder b)
+{
+    pl_str args = b->args;
+
+    b->output.len = 0;
+    for (int i = 0; i < b->templates.num; i++) {
+        size_t consumed = b->templates.elem[i](b, &b->output, args.buf);
+        pl_assert(consumed <= args.len);
+        args = pl_str_drop(args, consumed);
+    }
+
+    // Terminate with an extra \0 byte for convenience
+    grow_str(b, &b->output, b->output.len + 1);
+    b->output.buf[b->output.len] = '\0';
+    return b->output;
+}
+
+void pl_str_builder_append(pl_str_builder b, pl_str_template tmpl,
+                           const void *args, size_t size)
+{
+    PL_ARRAY_APPEND(b, b->templates, tmpl);
+    pl_str_append_raw(b, &b->args, args, size);
+}
+
+void pl_str_builder_concat(pl_str_builder b, const pl_str_builder append)
+{
+    PL_ARRAY_CONCAT(b, b->templates, append->templates);
+    pl_str_append_raw(b, &b->args, append->args.buf, append->args.len);
+}
+
+static size_t template_str_ptr(void *alloc, pl_str *buf, const void *args)
+{
+    const char * const *ptr = args;
+    pl_str_append_raw(alloc, buf, *ptr, strlen(*ptr));
+    return sizeof(*ptr);
+}
+
+void pl_str_builder_const_str(pl_str_builder b, const char *str)
+{
+    pl_str_builder_append(b, template_str_ptr, &str, sizeof(str));
+}
+
+struct str_arg {
+    size_t len;
+    uint8_t start[];
+};
+
+static size_t template_str(void *alloc, pl_str *buf, const void *args)
+{
+    const struct str_arg *arg = args;
+    pl_str_append_raw(alloc, buf, arg->start, arg->len);
+    return sizeof(*arg) + arg->len;
+}
+
+void pl_str_builder_str(pl_str_builder b, const pl_str str)
+{
+    struct str_arg arg = {
+        .len = str.len,
+    };
+
+    // Construct a `str_arg` struct inside `b->args` without actually
+    // constructing it by first appending an empty `str_arg` and then
+    // appending the pl_str directly into the args list.
+    pl_str_builder_append(b, template_str, &arg, sizeof(arg));
+    pl_str_append_raw(b, &b->args, str.buf, str.len);
+}
+
+void pl_str_builder_printf_c(pl_str_builder b, const char *fmt, ...)
+{
+    va_list ap;
+    va_start(ap, fmt);
+    pl_str_builder_vprintf_c(b, fmt, ap);
+    va_end(ap);
+}
+
+static size_t template_printf(void *alloc, pl_str *str, const void *args)
+{
+    const char * const *ptr = args;
+    return sizeof(*ptr) + pl_str_append_memprintf_c(alloc, str, *ptr, ptr + 1);
+}
+
+void pl_str_builder_vprintf_c(pl_str_builder b, const char *fmt, va_list ap)
+{
+    pl_str_builder_append(b, template_printf, &fmt, sizeof(fmt));
+
+    // Push all of the variadic arguments directly onto `b->args`
+    for (const char *c; (c = strchr(fmt, '%')) != NULL; fmt = c + 1) {
+        c++;
+        switch (c[0]) {
+#define WRITE(T, x) pl_str_append_raw(b, &b->args, &(T) {x}, sizeof(T))
+        case '%': continue;
+        case 'c': WRITE(char,       va_arg(ap, int)); break;
+        case 'd': WRITE(int,        va_arg(ap, int)); break;
+        case 'u': WRITE(unsigned,   va_arg(ap, unsigned)); break;
+        case 'f': WRITE(double,     va_arg(ap, double)); break;
+        case 'l':
+            assert(c[1] == 'l');
+            switch (c[2]) {
+            case 'u': WRITE(long long unsigned, va_arg(ap, long long unsigned)); break;
+            case 'd': WRITE(long long int,      va_arg(ap, long long int)); break;
+            default: abort();
+            }
+            c += 2;
+            break;
+        case 'z':
+            assert(c[1] == 'u');
+            WRITE(size_t, va_arg(ap, size_t));
+            c++;
+            break;
+        case 's': {
+            pl_str str = pl_str0(va_arg(ap, const char *));
+            pl_str_append(b, &b->args, str);
+            b->args.len++; // expand to include \0 byte (from pl_str_append)
+            break;
+        }
+        case '.': {
+            assert(c[1] == '*');
+            assert(c[2] == 's');
+            int len = va_arg(ap, int);
+            const char *str = va_arg(ap, const char *);
+            WRITE(int, len);
+            pl_str_append_raw(b, &b->args, str, len);
+            c += 2;
+            break;
+        }
+        default:
+            fprintf(stderr, "Invalid conversion character: '%c'!\n", c[0]);
+            abort();
+        }
+#undef WRITE
+    }
+}
