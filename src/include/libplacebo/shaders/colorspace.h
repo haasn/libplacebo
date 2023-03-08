haasn/libplacebo

commit 6b7e84c0afa19ec6efa4988ad9e2b2b8b385f2a9

shaders/colorspace: fix scene change hysteresis
The rewrite to peak detection made this logic happen on PQ, when it was
clearly intended to be calculated with raw nits values (hence the log
calls). Instead of using abs(log(new / old)), switch to abs(new - old)
in PQ space, which is already quasi-logarithmic.

Use the experimentally determined parameter 0.25f to convert from dB
gain to PQ delta, to roughly preserve backwards compatibility with the
option's documentation.

Fixes: daed681e04e7ccd36b1302940ec2c2001b9b67ee
Fixes: https://github.com/mpv-player/mpv/issues/11406

src/shaders/colorspace.c
@@ -1049,11 +1049,11 @@ static void update_peak_buf(pl_gpu gpu, struct sh_tone_map_obj *obj, bool force)
 
     // Scene change hysteresis
     if (params->scene_threshold_low > 0 && params->scene_threshold_high > 0) {
-        const float log_db = 10.0f / log(10.0f);
-        const float thresh_low = params->scene_threshold_low / log_db;
-        const float thresh_high = params->scene_threshold_high / log_db;
-        const float delta = fabsf(logf(avg_pq / obj->peak.avg_pq));
-        // smoothstep(thresh_low, thresh_high, thresh);
+        const float log10_pq = 0.25f; // experimentally determined approximate
+        const float thresh_low = params->scene_threshold_low * log10_pq;
+        const float thresh_high = params->scene_threshold_high * log10_pq;
+        const float delta = fabsf(avg_pq - obj->peak.avg_pq);
+        // smoothstep(thresh_low, thresh_high, delta);
         float thresh = (delta - thresh_low) / (thresh_high - thresh_low);
         thresh = PL_CLAMP(thresh, 0.0f, 1.0f);
         const float mix_coeff = thresh * thresh * (3.0f - 2.0f * thresh);

commit 20c074d84634f7fed7ef1e1a92983754a1331611

shaders/colorspace: fix scene change hysteresis (again)
This factor was accidentally off by an order of magnitude. It was also
too high, as a result of being estimated only for ratio 2 instead of
higher coefficients.

To be clear, this coefficient is supposed to be scaled such that `3 *
log10_pq` represents the pq delta that will result in a doubled signal
level, which is roughly 0.04 for low brightness levels. This turns out
to be almost 0.01 PQ, so we can remove the confusing (and misleading) dB
comment and just (correctly) document it as being scaled to 1% PQ steps.

Fixes: 6b7e84c0afa19ec6efa4988ad9e2b2b8b385f2a9
Fixes: https://github.com/mpv-player/mpv/issues/11406

src/include/libplacebo/shaders/colorspace.h
@@ -117,7 +117,7 @@ struct pl_peak_detect_params {
     // given threshold difference. But rather than a single hard cutoff, which
     // would lead to weird discontinuities on fades, we gradually disable it
     // over a small window of brightness ranges. These parameters control the
-    // lower and upper bounds of this window, in dB.
+    // lower and upper bounds of this window, in units of 1% PQ.
     //
     // The default values are 5.5 and 10.0, respectively. To disable this logic
     // entirely, set either one to a negative value.

src/shaders/colorspace.c
@@ -1069,7 +1069,7 @@ static void update_peak_buf(pl_gpu gpu, struct sh_tone_map_obj *obj, bool force)
 
     // Scene change hysteresis
     if (params->scene_threshold_low > 0 && params->scene_threshold_high > 0) {
-        const float log10_pq = 0.25f; // experimentally determined approximate
+        const float log10_pq = 1e-2f; // experimentally determined approximate
         const float thresh_low = params->scene_threshold_low * log10_pq;
         const float thresh_high = params->scene_threshold_high * log10_pq;
         const float delta = fabsf(avg_pq - obj->peak.avg_pq);