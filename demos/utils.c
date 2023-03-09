296c801819f7109f9a50dc719f829b6af9c4cb21
rcombs <rcombs@rcombs.me>
2023-03-08 20:42:44 -0600

    demos: fix build on macOS with older deployment targets
    clock_gettime() was added in macOS 10.12, and the default warnings require a __builtin_available check around use of any API newer than the current deployment target.

--- demos/utils.c
+++ demos/utils.c
@@ -17,8 +17,18 @@ bool utils_gettime(double *pTime)
     return true;
 #else
     struct timespec ts;
-    if (clock_gettime(CLOCK_MONOTONIC, &ts) < 0)
+
+#ifdef __APPLE__
+    if (__builtin_available(macOS 10.12, iOS 10.0, tvOS 10.0, watchOS 3.0, *))
+#else
+    if (true)
+#endif
+    {
+        if (clock_gettime(CLOCK_MONOTONIC, &ts) < 0)
+            return false;
+    } else {
         return false;
+    }
 
     *pTime = ts.tv_sec + ts.tv_nsec * 1e-9;
     return true;
