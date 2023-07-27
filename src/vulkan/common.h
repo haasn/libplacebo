haasn/libplacebo

commit 1d7092334dead779c42f15fd66b01e3086d472f6

vulkan/context: don't require synchronization2
Notably missing on MoltenVK. Work around it for now.

Fixes: https://github.com/haasn/libplacebo/issues/174
See-Also: https://github.com/KhronosGroup/MoltenVK/issues/1514

src/vulkan/context.c
@@ -231,6 +231,7 @@ static const VkPhysicalDeviceVulkan13Features recommended_vk13 = {
     .computeFullSubgroups = true,
     .maintenance4 = true,
     .shaderZeroInitializeWorkgroupMemory = true,
+    .synchronization2 = true,
 };
 
 static const VkPhysicalDeviceVulkan12Features recommended_vk12 = {
@@ -270,14 +271,8 @@ const VkPhysicalDeviceFeatures2 pl_vulkan_recommended_features = {
 };
 
 // Required features
-static const VkPhysicalDeviceVulkan13Features required_vk13 = {
-    .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES,
-    .synchronization2 = true,
-};
-
 static const VkPhysicalDeviceVulkan12Features required_vk12 = {
     .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES,
-    .pNext = (void *) &required_vk13,
     .hostQueryReset = true,
     .timelineSemaphore = true,
 };
@@ -304,7 +299,6 @@ static bool check_required_features(struct vk_ctx *vk)
         }                                                                       \
     } while (0)
 
-    CHECK_FEATURE(1, 3, synchronization2);
     CHECK_FEATURE(1, 2, hostQueryReset);
     CHECK_FEATURE(1, 2, timelineSemaphore);
 

commit d8e55344352755d95c5dedf60b22c34f26afd65d

vulkan: fix crash without synchronization2
No inherent need to use the sync2 command here because we don't require
the extra granularity provided by the stage2 mask, so we can just switch
to the pre-sync2 version of this command universally.

Fixes: 1d7092334dead779c42f15fd66b01e3086d472f6
Fixes: https://github.com/haasn/libplacebo/issues/182

src/vulkan/common.h
@@ -147,7 +147,7 @@ struct vk_ctx {
     PL_VK_FUN(CmdSetScissor);
     PL_VK_FUN(CmdSetViewport);
     PL_VK_FUN(CmdUpdateBuffer);
-    PL_VK_FUN(CmdWriteTimestamp2KHR);
+    PL_VK_FUN(CmdWriteTimestamp);
     PL_VK_FUN(CreateBuffer);
     PL_VK_FUN(CreateBufferView);
     PL_VK_FUN(CreateCommandPool);

src/vulkan/context.c
@@ -341,7 +341,7 @@ static const struct vk_fun vk_dev_funs[] = {
     PL_VK_DEV_FUN(CmdSetScissor),
     PL_VK_DEV_FUN(CmdSetViewport),
     PL_VK_DEV_FUN(CmdUpdateBuffer),
-    PL_VK_DEV_FUN(CmdWriteTimestamp2KHR),
+    PL_VK_DEV_FUN(CmdWriteTimestamp),
     PL_VK_DEV_FUN(CreateBuffer),
     PL_VK_DEV_FUN(CreateBufferView),
     PL_VK_DEV_FUN(CreateCommandPool),

src/vulkan/gpu.c
@@ -133,8 +133,8 @@ static void timer_begin(pl_gpu gpu, struct vk_cmd *cmd, pl_timer timer)
         vk->ResetQueryPool(vk->dev, timer->qpool, timer->index_write, 2);
     }
 
-    vk->CmdWriteTimestamp2KHR(cmd->buf, VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
-                              timer->qpool, timer->index_write);
+    vk->CmdWriteTimestamp(cmd->buf, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+                          timer->qpool, timer->index_write);
 
     p->cmd_timer = timer;
 }
@@ -207,8 +207,8 @@ bool _end_cmd(pl_gpu gpu, struct vk_cmd **pcmd, bool submit)
 
     if (p->cmd_timer) {
         pl_timer timer = p->cmd_timer;
-        vk->CmdWriteTimestamp2KHR(cmd->buf, VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT,
-                                  timer->qpool, timer->index_write + 1);
+        vk->CmdWriteTimestamp(cmd->buf, VK_PIPELINE_STAGE_ALL_COMMANDS_BIT,
+                              timer->qpool, timer->index_write + 1);
 
         timer->pending |= timer_bit(timer->index_write);
         vk_cmd_callback(cmd, (vk_cb) timer_end_cb, timer,