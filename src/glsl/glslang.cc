haasn/libplacebo

commit 9c693a5a1762fc83078f85933f0532e2098d3254

glsl/glslang: enable optimizer
This helps a little with SPIR-V code generation.

src/glsl/glslang.cc
@@ -104,8 +104,18 @@ struct pl_glslang_res *pl_glslang_compile(const struct pl_glsl_version *glsl,
         return res;
     }
 
+    SpvOptions options = {
+        .generateDebugInfo = false,
+        .stripDebugInfo = true,
+        .disableOptimizer = false,
+        .optimizeSize = true,
+        .disassemble = false,
+        .validate = true,
+        .emitNonSemanticShaderDebugInfo = false,
+        .emitNonSemanticShaderDebugSource = false,
+    };
     std::vector<unsigned int> spirv;
-    GlslangToSpv(*prog->getIntermediate(lang), spirv);
+    GlslangToSpv(*prog->getIntermediate(lang), spirv, &options);
 
     res->success = true;
     res->size = spirv.size() * sizeof(unsigned int);

commit 648cc72c739a78e38dee0ef3acefc78ff3083def

glsl/glslang: avoid use of designated initializer
../src/glsl/glslang.cc:116:5: error: designated initializers cannot be used
with a non-aggregate type ‘glslang::SpvOptions’

Fixes: 9c693a5a1762fc83078f85933f0532e2098d3254

src/glsl/glslang.cc
@@ -104,16 +104,11 @@ struct pl_glslang_res *pl_glslang_compile(const struct pl_glsl_version *glsl,
         return res;
     }
 
-    SpvOptions options = {
-        .generateDebugInfo = false,
-        .stripDebugInfo = true,
-        .disableOptimizer = false,
-        .optimizeSize = true,
-        .disassemble = false,
-        .validate = true,
-        .emitNonSemanticShaderDebugInfo = false,
-        .emitNonSemanticShaderDebugSource = false,
-    };
+    SpvOptions options;
+    options.disableOptimizer = false;
+    options.stripDebugInfo = true;
+    options.optimizeSize = true;
+    options.validate = true;
     std::vector<unsigned int> spirv;
     GlslangToSpv(*prog->getIntermediate(lang), spirv, &options);
 