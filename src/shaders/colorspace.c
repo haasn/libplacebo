haasn/libplacebo

commit 31082eee7a6dc800df5bd7db6d115efcaaa2dd8c

shaders/colorspace: exclude black pixels from peak detection
Significantly improves peak detection result on frames containing many
black pixels (e.g. hardcoded black bars). In real content, truly black
pixels are incredibly rare, so this is pretty safe to do.

Only requires one extra group shared atomic and some once-per-WG code. I
briefly played with the idea of including this implicitly in the
histogram measurement, but it was more complicated to do so and limits
it to the case of histogram being enabled.

src/shaders/colorspace.c
@@ -1186,7 +1186,7 @@ bool pl_shader_detect_peak(pl_shader sh, struct pl_color_space csp,
     }
 
     const bool use_histogram = params->percentile > 0 && params->percentile < 100;
-    size_t shmem_req = 2 * sizeof(uint32_t);
+    size_t shmem_req = 3 * sizeof(uint32_t);
     if (use_histogram)
         shmem_req += sizeof(uint32_t[HIST_BINS]);
 
@@ -1270,10 +1270,11 @@ retry_ssbo:
 
     // For performance, we want to do as few atomic operations on global
     // memory as possible, so use an atomic in shmem for the work group.
-    ident_t wg_sum = sh_fresh(sh, "wg_sum"),
-            wg_max = sh_fresh(sh, "wg_max"),
-            wg_hist = NULL_IDENT;
-    GLSLH("shared uint "$", "$"; \n", wg_sum, wg_max);
+    ident_t wg_sum   = sh_fresh(sh, "wg_sum"),
+            wg_max   = sh_fresh(sh, "wg_max"),
+            wg_black = sh_fresh(sh, "wg_black"),
+            wg_hist  = NULL_IDENT;
+    GLSLH("shared uint "$", "$", "$"; \n", wg_sum, wg_max, wg_black);
     if (use_histogram) {
         wg_hist = sh_fresh(sh, "wg_hist");
         GLSLH("shared uint "$"[%u]; \n", wg_hist, HIST_BINS);
@@ -1281,9 +1282,9 @@ retry_ssbo:
              "    "$"[i] = 0u;                                              \n",
              HIST_BINS, wg_hist);
     }
-    GLSL($" = 0u; "$" = 0u; \n"
-         "barrier();        \n",
-         wg_sum, wg_max);
+    GLSL($" = 0u; "$" = 0u; "$" = 0u; \n"
+         "barrier();                  \n",
+         wg_sum, wg_max, wg_black);
 
     // Decode color into linear light representation
     pl_color_space_infer(&csp);
@@ -1323,36 +1324,45 @@ retry_ssbo:
     }
 
     if (has_subgroups) {
-        GLSL("uint group_sum = subgroupAdd(y_pq);   \n"
-             "uint group_max = subgroupMax(y_pq);   \n"
-             "if (subgroupElect()) {                \n"
-             "    atomicAdd("$", group_sum);        \n"
-             "    atomicMax("$", group_max);        \n"
-             "}                                     \n"
-             "barrier();                            \n",
-             wg_sum, wg_max);
+        GLSL("uint group_sum = subgroupAdd(y_pq);           \n"
+             "uint group_max = subgroupMax(y_pq);           \n"
+             "uvec4 b = subgroupBallot(y_pq == 0u);         \n"
+             "if (subgroupElect()) {                        \n"
+             "    atomicAdd("$", group_sum);                \n"
+             "    atomicMax("$", group_max);                \n"
+             "    atomicAdd("$", subgroupBallotBitCount(b));\n"
+             "}                                             \n"
+             "barrier();                                    \n",
+             wg_sum, wg_max, wg_black);
     } else {
-        GLSL("atomicAdd("$", y_pq); \n"
-             "atomicMax("$", y_pq); \n"
-             "barrier();            \n",
-             wg_sum, wg_max);
+        GLSL("atomicAdd("$", y_pq);     \n"
+             "atomicMax("$", y_pq);     \n"
+             "if (y_pq == 0u)           \n"
+             "    atomicAdd("$", 1u);   \n"
+             "barrier();                \n",
+             wg_sum, wg_max, wg_black);
     }
 
     if (use_histogram) {
-        GLSL("for (uint i = gl_LocalInvocationIndex; i < %du; i += wg_size) \n"
+        GLSL("if (gl_LocalInvocationIndex == 0u)                            \n"
+             "    "$"[0] -= "$";                                            \n"
+             "for (uint i = gl_LocalInvocationIndex; i < %du; i += wg_size) \n"
              "    atomicAdd(frame_hist[slice * %du + i], "$"[i]);           \n",
-             HIST_BINS, HIST_BINS, wg_hist);
+             wg_hist, wg_black,
+             HIST_BINS,
+             HIST_BINS, wg_hist);
     }
 
     // Have one thread per work group update the global atomics
-    GLSL("if (gl_LocalInvocationIndex == 0u) {              \n"
-         "    atomicAdd(frame_wg_count[slice], 1u);         \n"
-         "    atomicAdd(frame_sum_pq[slice], "$" / wg_size);\n"
-         "    atomicMax(frame_max_pq[slice], "$");          \n"
-         "}                                                 \n"
-         "color = color_orig;                               \n"
-         "}                                                 \n",
-          wg_sum, wg_max);
+    GLSL("if (gl_LocalInvocationIndex == 0u && "$" < wg_size) { \n"
+         "    uint num = wg_size - "$";                         \n"
+         "    atomicAdd(frame_wg_count[slice], 1u);             \n"
+         "    atomicAdd(frame_sum_pq[slice], "$" / num);        \n"
+         "    atomicMax(frame_max_pq[slice], "$");              \n"
+         "}                                                     \n"
+         "color = color_orig;                                   \n"
+         "}                                                     \n",
+          wg_black, wg_black, wg_sum, wg_max);
 
     return true;
 }

commit 08f76b7892ea96c3b9f131b81a55375a7a216853

shaders/colorspace: don't warn on solid black frame
Since 31082eee7a it's possible for frame_wg_count to not get
incremented, in particular, for all black WGs. This may lead to the
scenario where frame_wg_count is 0 for the frame/slice. Avoid false
positive on the allow_delayed check and also handle this case correctly
by introducing explicit `frame_ready` field which is set to 1 once per
compute shader invocation.

Fixes: 31082eee7a6dc800df5bd7db6d115efcaaa2dd8c

src/shaders/colorspace.c
@@ -973,6 +973,7 @@ enum {
 pl_static_assert(PQ_BITS >= HIST_BITS);
 
 struct peak_buf_data {
+    unsigned frame_ready;
     unsigned frame_wg_count[SLICES]; // number of work groups processed
     unsigned frame_sum_pq[SLICES];   // sum of PQ Y values over all WGs (PQ_BITS)
     unsigned frame_max_pq[SLICES];   // maximum PQ Y value among these WGs (PQ_BITS)
@@ -995,6 +996,7 @@ static const struct pl_buffer_var peak_buf_vars[] = {
         .stride = sizeof(unsigned),                                             \
     },                                                                          \
 }
+    VAR(frame_ready),
     VAR(frame_wg_count),
     VAR(frame_sum_pq),
     VAR(frame_max_pq),
@@ -1111,7 +1113,7 @@ static void update_peak_buf(pl_gpu gpu, struct sh_color_map_obj *obj, bool force
     } else {
         ok = pl_buf_read(gpu, obj->peak.buf, 0, &data, sizeof(data));
     }
-    if (ok && data.frame_wg_count[0] > 0) {
+    if (ok && data.frame_ready) {
         // Peak detection completed successfully
         pl_buf_destroy(gpu, &obj->peak.buf);
     } else {
@@ -1136,6 +1138,12 @@ static void update_peak_buf(pl_gpu gpu, struct sh_color_map_obj *obj, bool force
         frame_sum_pq   += data.frame_sum_pq[k];
         frame_wg_count += data.frame_wg_count[k];
     }
+    if (!frame_wg_count) {
+        // Solid black frame
+        obj->peak.avg_pq = obj->peak.max_pq = PL_COLOR_HDR_BLACK;
+        return;
+    }
+
     float avg_pq = (float) frame_sum_pq / (frame_wg_count * PQ_MAX);
     float max_pq = measure_peak(&data, params->percentile);
 
@@ -1360,6 +1368,8 @@ retry_ssbo:
          "    atomicAdd(frame_sum_pq[slice], "$" / num);        \n"
          "    atomicMax(frame_max_pq[slice], "$");              \n"
          "}                                                     \n"
+         "if (gl_GlobalInvocationID == vec3(0u))                \n"
+         "    frame_ready = 1u;                                 \n"
          "color = color_orig;                                   \n"
          "}                                                     \n",
           wg_black, wg_black, wg_sum, wg_max);