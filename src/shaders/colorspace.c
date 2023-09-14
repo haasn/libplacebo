haasn/libplacebo

commit 66c555b9c105139dc77578a02369cce5210f2333

shaders/colorspace: switch to UNORM 3DLUT texture
This is actually much more efficient, because we were actually only
using the range [-0.5,0.5] anyway. So just shifting that allows us to
use the full range of UNORM trivially.

Also updates the cache key, which should have been done in d548232d.

src/shaders/colorspace.c
@@ -1647,16 +1647,15 @@ static void fill_gamut_lut(void *data, const struct sh_lut_params *params)
     void *tmp = pl_alloc(NULL, lut_size * sizeof(float) * lut_params->lut_stride);
     pl_gamut_map_generate(tmp, lut_params);
 
-    // Convert to 16-bit signed integer for GPU texture
+    // Convert to 16-bit unsigned integer for GPU texture
     const float *in = tmp;
-    int16_t *out = data;
+    uint16_t *out = data;
     pl_assert(lut_params->lut_stride == 3);
     pl_assert(params->comps == 4);
     for (int i = 0; i < lut_size; i++) {
-        const int16_t range = INT16_MAX - 1;
-        out[0] = roundf(in[0] * range);
-        out[1] = roundf(in[1] * range);
-        out[2] = roundf(in[2] * range);
+        out[0] = roundf(in[0] * UINT16_MAX);
+        out[1] = roundf((in[1] + 0.5f) * UINT16_MAX);
+        out[2] = roundf((in[2] + 0.5f) * UINT16_MAX);
         in  += 3;
         out += 4;
     }
@@ -1807,7 +1806,7 @@ void pl_shader_color_map_ex(pl_shader sh, const struct pl_color_map_params *para
             gamut.function = &pl_gamut_map_saturation;
     }
 
-    pl_fmt gamut_fmt = pl_find_fmt(SH_GPU(sh), PL_FMT_SNORM, 4, 16, 16, PL_FMT_CAP_LINEAR);
+    pl_fmt gamut_fmt = pl_find_fmt(SH_GPU(sh), PL_FMT_UNORM, 4, 16, 16, PL_FMT_CAP_LINEAR);
     if (!gamut_fmt) {
         gamut.function = &pl_gamut_map_saturation;
         can_fast = true;
@@ -2014,7 +2013,8 @@ void pl_shader_color_map_ex(pl_shader sh, const struct pl_color_map_params *para
              "idx.x = "$" * ipt.x + "$";            \n"
              "idx.y = 2.0 * length(ipt.yz);         \n"
              "idx.z = %f * atan(ipt.z, ipt.y) + 0.5;\n"
-             "ipt = "$"(idx).xyz;                   \n",
+             "ipt = "$"(idx).xyz;                   \n"
+             "ipt.yz -= vec2(0.5);                  \n",
              SH_FLOAT(1.0f / lut_range),
              SH_FLOAT(-gamut.min_luma / lut_range),
              0.5f / M_PI, lut);

src/cache.h
@@ -64,7 +64,7 @@ enum {
     CACHE_KEY_ICC_3DLUT = UINT64_C(0xff703a6dd8a996f6), // ICC 3dlut
     CACHE_KEY_DITHER    = UINT64_C(0x6fed75eb6dce86cb), // dither matrix
     CACHE_KEY_H274      = UINT64_C(0x2fb9adca04b42c4d), // H.274 film grain DB
-    CACHE_KEY_GAMUT_LUT = UINT64_C(0x41bbe0c35ea24b2e), // gamut mapping 3DLUT
+    CACHE_KEY_GAMUT_LUT = UINT64_C(0x6109e47f15d478b1), // gamut mapping 3DLUT
     CACHE_KEY_SPIRV     = UINT64_C(0x32352f6605ff60a7), // bare SPIR-V module
     CACHE_KEY_VK_PIPE   = UINT64_C(0x4bdab2817ad02ad4), // VkPipelineCache
     CACHE_KEY_GL_PROG   = UINT64_C(0x4274c309f4f0477b), // GL_ARB_get_program_binary

commit c9012ae8c09c96da55410b20e3cb1f4e91271e80

shaders/colorspace: fix gamut visualization
Fixes: 66c555b9c105139dc77578a02369cce5210f2333

src/shaders/colorspace.c
@@ -1588,6 +1588,7 @@ static void visualize_gamut_map(pl_shader sh, pl_rect2df rc,
          "idx.y = 2.0 * length(ipt.yz);                         \n"
          "idx.z = %f * atan(ipt.z, ipt.y) + 0.5;                \n"
          "vec3 mapped = "$"(idx).xyz;                           \n"
+         "mapped.yz -= vec2(0.5);                               \n"
          "float mappedhue = atan(mapped.z, mapped.y);           \n"
          "float mappedchroma = length(mapped.yz);               \n"
          "ipt = mapped;                                         \n"