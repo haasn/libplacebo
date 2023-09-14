haasn/libplacebo

commit b38e1c84278e165aafa90101292d40c17e7940a0

demos/plplay: add basic shader caching support
Probably won't work for ICC/3DLUT cache but these probably want their
own custom mechanism anyway, since they're quite large.

demos/plplay.c
@@ -15,6 +15,7 @@
 
 #include "common.h"
 #include "window.h"
+#include "utils.h"
 #include "pl_clock.h"
 #include "pl_thread.h"
 
@@ -50,11 +51,13 @@ static bool ui_draw(struct ui *ui, const struct pl_swapchain_frame *frame) { ret
 struct plplay {
     struct window *win;
     struct ui *ui;
+    char cache_file[512];
 
     // libplacebo
     pl_log log;
     pl_renderer renderer;
     pl_queue queue;
+    pl_cache cache;
 
     // libav*
     AVFormatContext *format;
@@ -154,6 +157,15 @@ static void uninit(struct plplay *p)
     free(p->shader_paths);
     free(p->target_icc_name);
     av_file_unmap((void *) p->target_icc.data, p->target_icc.len);
+
+    if (p->cache) {
+        FILE *file = fopen(p->cache_file, "wb");
+        if (file) {
+            pl_cache_save_file(p->cache, file);
+            fclose(file);
+        }
+        pl_cache_destroy(&p->cache);
+    }
 
     // Free this before destroying the window to release associated GPU buffers
     avcodec_free_context(&p->codec);
@@ -820,6 +832,24 @@ int main(int argc, char **argv)
 
     if (!init_codec(p))
         goto error;
+
+    const char *cache_dir = get_cache_dir(&(char[512]) {0});
+    if (cache_dir) {
+        int ret = snprintf(p->cache_file, sizeof(p->cache_file), "%s/plplay.cache", cache_dir);
+        if (ret > 0 && ret < sizeof(p->cache_file)) {
+            p->cache = pl_cache_create(pl_cache_params(
+                .log             = p->log,
+                .max_object_size =  1 << 20, //  1 MB
+                .max_total_size  = 10 << 20, // 10 MB
+            ));
+            pl_gpu_set_cache(p->win->gpu, p->cache);
+            FILE *file = fopen(p->cache_file, "rb");
+            if (file) {
+                pl_cache_load_file(p->cache, file);
+                fclose(file);
+            }
+        }
+    }
 
     p->queue = pl_queue_create(p->win->gpu);
     int ret = pl_thread_create(&p->decoder_thread, decode_loop, p);

commit c1f0e151a11baaacdadf5fd7744ecad2f30b9f94

demos/plplay: fix target ICC signature calculation
Addition of the pl_cache means we now persist ICC 3DLUTs across
restarts, in which case we seriously need to use the actual hash of the
profile to identify ICC profiles. Otherwise there will be cache
collisions all over the place.

Fixes: b38e1c84278e165aafa90101292d40c17e7940a0

demos/plplay.c
@@ -1799,7 +1799,7 @@ static void update_settings(struct plplay *p, const struct pl_frame *target)
                     av_file_unmap((void *) p->target_icc.data, p->target_icc.len);
                     p->target_icc.data = iccbuf;
                     p->target_icc.len = size;
-                    p->target_icc.signature++;
+                    pl_icc_profile_compute_signature(&p->target_icc);
                     free(p->target_icc_name);
                     p->target_icc_name = strdup(PL_BASENAME((char *) dropped_file));
                 }
@@ -1827,9 +1827,7 @@ static void update_settings(struct plplay *p, const struct pl_frame *target)
                 av_file_unmap((void *) p->target_icc.data, p->target_icc.len);
                 free(p->target_icc_name);
                 p->target_icc_name = NULL;
-                p->target_icc = (struct pl_icc_profile) {
-                    .signature = p->target_icc.signature + 1,
-                };
+                p->target_icc = (struct pl_icc_profile) {0};
             }
 
             nk_tree_pop(nk);