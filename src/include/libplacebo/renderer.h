haasn/libplacebo

commit f87df67f892c7031ed80ef72dd169dc2a7860aa2

renderer: remove pl_render_params.ignore_icc_profiles
Should really have been part of the previous API bump, but oh well. We
can't support this easily in the pl_frames_infer() case, and it's also
not used by any downstream client currently. It's much simpler to just
set ICC profiles to NULL.

src/include/libplacebo/renderer.h
@@ -314,9 +314,6 @@ struct pl_render_params {
     // Forces correction of subpixel offsets (using the configured `upscaler`).
     bool correct_subpixel_offsets;
 
-    // Ignore ICC profiles attached to either `image` or `target`.
-    bool ignore_icc_profiles;
-
     // Forces the use of dithering, even when rendering to 16-bit FBOs. This is
     // generally pretty pointless because most 16-bit FBOs have high enough
     // depth that rounding errors are below the human perception threshold,
@@ -355,6 +352,7 @@ struct pl_render_params {
     // --- Deprecated/removed fields
     bool allow_delayed_peak_detect PL_DEPRECATED; // moved to pl_peak_detect_params
     const struct pl_icc_params *icc_params PL_DEPRECATED; // use pl_frame.icc
+    bool ignore_icc_profiles; // non-functional, just set pl_frame.icc to NULL
 };
 
 // Bare minimum parameters, with no features enabled. This is the fastest

demos/plplay.c
@@ -1957,7 +1957,6 @@ static void update_settings(struct plplay *p, const struct pl_frame *target)
             nk_checkbox_label(nk, "Disable FBOs / advanced rendering", &par->disable_fbos);
             nk_checkbox_label(nk, "Force low-bit depth FBOs", &par->force_low_bit_depth_fbos);
             nk_checkbox_label(nk, "Disable constant hard-coding", &par->dynamic_constants);
-            nk_checkbox_label(nk, "Ignore ICC profiles", &par->ignore_icc_profiles);
 
             if (nk_check_label(nk, "Ignore Dolby Vision metadata", p->ignore_dovi) != p->ignore_dovi) {
                 // Flush the renderer cache on changes, since this can

meson.build
@@ -12,6 +12,7 @@ project('libplacebo', ['c', 'cpp'],
     6,
     # API version
     {
+      '328': 'remove pl_render_params.ignore_icc_profiles',
       '327': 'remove pl_render_params.icc_params',
       '326': 'add pl_frame.icc',
       '325': 'add pl_icc_update',

src/options.c
@@ -1144,7 +1144,7 @@ const struct pl_opt_t pl_option_list[] = {
     OPT_BOOL("disable_linear_scaling", "Disable linear scaling", params.disable_linear_scaling),
     OPT_BOOL("disable_builtin_scalers", "Disable built-in scalers", params.disable_builtin_scalers),
     OPT_BOOL("correct_subpixel_offset", "Correct subpixel offsets", params.correct_subpixel_offsets),
-    OPT_BOOL("ignore_icc_profiles", "Ignore ICC profiles", params.ignore_icc_profiles),
+    OPT_BOOL("ignore_icc_profiles", "Ignore ICC profiles", params.ignore_icc_profiles, .deprecated = true),
     OPT_BOOL("force_dither", "Force-enable dithering", params.force_dither),
     OPT_BOOL("disable_dither_gamma_correction", "Disable gamma-correct dithering", params.disable_dither_gamma_correction),
     OPT_BOOL("disable_fbos", "Disable FBOs", params.disable_fbos),

src/renderer.c
@@ -2910,23 +2910,19 @@ static bool pass_init(struct pass_state *pass, bool acquire_image)
     fix_refs_and_rects(pass);
     find_fbo_format(pass);
 
-    if (params->ignore_icc_profiles) {
-        image->icc = target->icc = NULL;
-    } else {
-        // Fallback for older ICC profile API
-        icc_fallback(pass, image,  &rr->icc_fallback[ICC_IMAGE]);
-        icc_fallback(pass, target, &rr->icc_fallback[ICC_TARGET]);
+    // Fallback for older ICC profile API
+    icc_fallback(pass, image,  &rr->icc_fallback[ICC_IMAGE]);
+    icc_fallback(pass, target, &rr->icc_fallback[ICC_TARGET]);
 
-        // Force colorspace metadata to ICC profile values, if present
-        if (image && image->icc) {
-            image->color.primaries = image->icc->containing_primaries;
-            image->color.hdr = image->icc->csp.hdr;
-        }
+    // Force colorspace metadata to ICC profile values, if present
+    if (image && image->icc) {
+        image->color.primaries = image->icc->containing_primaries;
+        image->color.hdr = image->icc->csp.hdr;
+    }
 
-        if (target->icc) {
-            target->color.primaries = target->icc->containing_primaries;
-            target->color.hdr = target->icc->csp.hdr;
-        }
+    if (target->icc) {
+        target->color.primaries = target->icc->containing_primaries;
+        target->color.hdr = target->icc->csp.hdr;
     }
 
     // Infer the target color space info based on the image's

commit c29169ba50544f96de7f33c7759bce9d3cffa313

renderer: add missing deprecation tag
Fixes: f87df67f892c7031ed80ef72dd169dc2a7860aa2

src/include/libplacebo/renderer.h
@@ -352,7 +352,7 @@ struct pl_render_params {
     // --- Deprecated/removed fields
     bool allow_delayed_peak_detect PL_DEPRECATED; // moved to pl_peak_detect_params
     const struct pl_icc_params *icc_params PL_DEPRECATED; // use pl_frame.icc
-    bool ignore_icc_profiles; // non-functional, just set pl_frame.icc to NULL
+    bool ignore_icc_profiles PL_DEPRECATED; // non-functional, just set pl_frame.icc to NULL
 };
 
 // Bare minimum parameters, with no features enabled. This is the fastest