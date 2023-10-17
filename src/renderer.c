ee2238adc996ed8e74dd9c48f09a8601400ad235
Niklas Haas <git@haasn.dev>
2023-10-17 12:46:43 +0200

    renderer: properly clamp tscale blur calculation
    This accidentally sharpened the frame mixer when the source fps was
    below monitor fps.

--- src/renderer.c
+++ src/renderer.c
@@ -3309,7 +3309,7 @@ bool pl_render_image_mix(pl_renderer rr, const struct pl_frame_mix *images,
         for (int i = 1; i < images->num_frames; i++) {
             if (images->timestamps[i] >= 0.0 && images->timestamps[i - 1] < 0) {
                 float frame_dur = images->timestamps[i] - images->timestamps[i - 1];
-                if (!params->skip_anti_aliasing)
+                if (images->vsync_duration > frame_dur && !params->skip_anti_aliasing)
                     mixer.blur *= images->vsync_duration / frame_dur;
                 break;
             }
