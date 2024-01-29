haasn/libplacebo

commit a5e701025dce2036930b9e049862e5355b07a45f

utils/libav: Only adjust bit_shift for DRM P010 frames
Fixes mapping Vulkan P010 frames.

Closes #237

src/include/libplacebo/utils/libav_internal.h
@@ -967,11 +967,6 @@ static void pl_fix_hwframe_sample_depth(struct pl_frame *out, const AVFrame *fra
     struct pl_bit_encoding *bits = &out->repr.bits;
 
     bits->sample_depth = fmt->component_depth[0];
-
-    switch (hwfc->sw_format) {
-    case AV_PIX_FMT_P010: bits->bit_shift = 6; break;
-    default: break;
-    }
 }
 
 static bool pl_map_avframe_drm(pl_gpu gpu, struct pl_frame *out,
@@ -1016,6 +1011,12 @@ static bool pl_map_avframe_drm(pl_gpu gpu, struct pl_frame *out,
     }
 
     pl_fix_hwframe_sample_depth(out, frame);
+
+    switch (hwfc->sw_format) {
+    case AV_PIX_FMT_P010: out->repr.bits.bit_shift = 6; break;
+    default: break;
+    }
+
     return true;
 }
 

commit 6d203e537a92e0144a070e712d34f602c09186de

utils/libav: fix unused variable warning
Fixes: a5e701025dce2036930b9e049862e5355b07a45f

src/include/libplacebo/utils/libav_internal.h
@@ -962,10 +962,8 @@ struct pl_avframe_priv {
 
 static void pl_fix_hwframe_sample_depth(struct pl_frame *out, const AVFrame *frame)
 {
-    const AVHWFramesContext *hwfc = (AVHWFramesContext *) frame->hw_frames_ctx->data;
     pl_fmt fmt = out->planes[0].texture->params.format;
     struct pl_bit_encoding *bits = &out->repr.bits;
-
     bits->sample_depth = fmt->component_depth[0];
 }
 