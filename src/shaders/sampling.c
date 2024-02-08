haasn/libplacebo

commit b2b61a755921499e2075d409a9eb667232be194b

shaders/sampling: don't unroll whole ortho sampling
Unrolling fully doesn't bring performance gain and makes shaders huge
and heavy to compile.

Also this avoid recompiling shader when kernel size changes, because it
is not dynamic variable.

Comparision on Ryzen 7950x.

Before:

Spent 27.292 ms translating SPIR-V
Spent 1317.319 ms translating SPIR-V (slow!)
Spent 0.052 ms compiling shader
Spent 67.641 ms creating pipeline

After:

Spent 28.963 ms translating SPIR-V
Spent 77.494 ms translating SPIR-V
Spent 0.043 ms compiling shader
Spent 0.199 ms creating pipeline

Note it is done twice, for vertical and horizontal kernel. On top on
some platforms there is additional step to translate SPIR-V to another
format like DXBC.

In an atempt to mitigate some users having excessive compilation times
for no good reason.

See: https://github.com/mpv-player/mpv/issues/12753

src/shaders/sampling.c
@@ -1054,27 +1054,26 @@ bool pl_shader_sample_ortho2(pl_shader sh, const struct pl_sample_src *src,
         ${vecType: comps} hi = ${vecType: comps}(0.0);                          \
         ${vecType: comps} lo = ${vecType: comps}(1e9);                          \
     @}                                                                          \
-    @for (n < N) {                                                              \
-        @if @(n % 4 == 0)                                                       \
-            ws = $lut(vec2(float(@n / 4) / ${const float: denom}, fcoord));     \
-        @if @(vars.use_ar && (n == vars.n / 2 - 1 || n == vars.n / 2)) {        \
-            c = textureLod($src_tex, base + pt * @n.0, 0.0).${swizzle: comps};  \
-            ca += ws[@n % 4] * c;                                               \
-            lo = min(lo, c);                                                    \
-            hi = max(hi, c);                                                    \
+    #pragma unroll 4                                                            \
+    for (int n = 0; n < ${int: N}; ++n) {                                       \
+        if (n % 4 == 0)                                                         \
+            ws = $lut(vec2(float(n / 4) / ${const float: denom}, fcoord));      \
+        @if (use_linear) {                                                      \
+            if (n % 2 == 0) {                                                   \
+                off = float(n) + ws[n % 4 + 1];                                 \
+                c = textureLod($src_tex, base + pt * off, 0.0).${swizzle: comps}; \
+            }                                                                   \
         @} else {                                                               \
-            @if (use_linear) {                                                  \
-                @if @(n % 2 == 0) {                                             \
-                    off = @n.0 + ws[@n % 4 + 1];                                \
-                    ca += ws[@n % 4] * textureLod($src_tex, base + pt * off,    \
-                                                  0.0).${swizzle: comps};       \
-                @}                                                              \
-            @} else {                                                           \
-                ca += ws[@n % 4] * textureLod($src_tex, base + pt * @n.0,       \
-                                              0.0).${swizzle: comps};           \
-            @}                                                                  \
+            c = textureLod($src_tex, base + pt * float(n), 0.0).${swizzle: comps}; \
         @}                                                                      \
-    @}                                                                          \
+        @if (use_ar) {                                                          \
+            if (n == ${int: N} / 2 - 1 || n == ${int: N} / 2) {                 \
+                lo = min(lo, c);                                                \
+                hi = max(hi, c);                                                \
+            }                                                                   \
+        @}                                                                      \
+        ca += ws[n % 4] * c;                                                    \
+    }                                                                           \
     @if (use_ar)                                                                \
         ca = mix(ca, clamp(ca, lo, hi), ${float: cfg.antiring});                \
     color.${swizzle: comps} = ${float: scale} * ca;                             \

commit fab08fb85e18896e4feb43abdc5edacf9571d389

shaders/sampling: add proper loop step for use_linear case
Fixes: b2b61a75

src/shaders/sampling.c
@@ -1055,19 +1055,17 @@ bool pl_shader_sample_ortho2(pl_shader sh, const struct pl_sample_src *src,
         ${vecType: comps} lo = ${vecType: comps}(1e9);                          \
     @}                                                                          \
     #pragma unroll 4                                                            \
-    for (int n = 0; n < ${int: N}; ++n) {                                       \
+    for (int n = 0; n < ${int: N}; n += ${const int: use_linear ? 2 : 1}) {     \
         if (n % 4 == 0)                                                         \
             ws = $lut(vec2(float(n / 4) / ${const float: denom}, fcoord));      \
         @if (use_linear) {                                                      \
-            if (n % 2 == 0) {                                                   \
-                off = float(n) + ws[n % 4 + 1];                                 \
-                c = textureLod($src_tex, base + pt * off, 0.0).${swizzle: comps}; \
-            }                                                                   \
+            off = float(n) + ws[n % 4 + 1];                                     \
+            c = textureLod($src_tex, base + pt * off, 0.0).${swizzle: comps};   \
         @} else {                                                               \
             c = textureLod($src_tex, base + pt * float(n), 0.0).${swizzle: comps}; \
         @}                                                                      \
         @if (use_ar) {                                                          \
-            if (n == ${int: N} / 2 - 1 || n == ${int: N} / 2) {                 \
+            if (n == ${uint: N} / 2 - 1 || n == ${uint: N} / 2) {               \
                 lo = min(lo, c);                                                \
                 hi = max(hi, c);                                                \
             }                                                                   \