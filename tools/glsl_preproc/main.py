d375b4c72ca00d3f4de7c34ea3205c44f0d3833d
Kacper Michajłow <kasper93@gmail.com>
2024-03-07 07:18:34 +0100

    tools/glsl_preproc: force utf-8 input
    Commit 2f06c045 did output only, but we should use utf-8 for both.

--- tools/glsl_preproc/main.py
+++ tools/glsl_preproc/main.py
@@ -11,7 +11,7 @@ parser.add_argument('output')
 parser.add_argument('-s', '--strip', default=False, action='store_true')
 args = parser.parse_args()
 
-with open(args.input) as infile:
+with open(args.input, encoding='utf-8') as infile:
     with open(args.output, 'w', encoding='utf-8') as outfile:
         for line in Macro.process_file(infile, strip=args.strip):
             outfile.write(line)
