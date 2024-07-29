3a8d05161b8a69175c9bd6c4f73372c022a8ad6d
Niklas Haas <git@haasn.dev>
2024-07-29 12:09:39 +0200

    tests/fuzz: fix header location
    This was renamed in the past, but the fuzz test cases were never updated
    as a result.
    
    Partially-fixes: https://code.videolan.org/videolan/libplacebo/-/issues/326

--- src/tests/fuzz/lut.c
+++ src/tests/fuzz/lut.c
@@ -1,4 +1,4 @@
-#include "../tests.h"
+#include "../utils.h"
 
 #include <libplacebo/shaders/lut.h>
 
--- src/tests/fuzz/shaders.c
+++ src/tests/fuzz/shaders.c
@@ -1,4 +1,4 @@
-#include "../tests.h"
+#include "../utils.h"
 #include "shaders.h"
 
 #include <libplacebo/dummy.h>
