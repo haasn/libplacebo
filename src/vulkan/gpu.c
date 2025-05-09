4c6d99edee23284f93b07f0f045cd660327465eb
Kacper Michajłow <kasper93@gmail.com>
2025-05-09 18:51:28 +0200

    vulkan/gpu: set correct pl_spirv_version for Vulkan 1.4
    maintenance4 is not present in Vulkan 1.4, so we were skipping this
    condition and falling back to Vulkan 1.2 case. Add explicit 1.4 check to
    fix that.

--- src/vulkan/gpu.c
+++ src/vulkan/gpu.c
@@ -386,6 +386,13 @@ static const VkFilter filters[PL_TEX_SAMPLE_MODE_COUNT] = {
 
 static inline struct pl_spirv_version get_spirv_version(const struct vk_ctx *vk)
 {
+    if (vk->api_ver >= VK_API_VERSION_1_4) {
+        return (struct pl_spirv_version) {
+            .env_version = VK_API_VERSION_1_4,
+            .spv_version = PL_SPV_VERSION(1, 6),
+        };
+    }
+
     if (vk->api_ver >= VK_API_VERSION_1_3) {
         const VkPhysicalDeviceMaintenance4Features *device_maintenance4;
         device_maintenance4 = vk_find_struct(&vk->features,
