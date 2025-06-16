haasn/libplacebo

commit 0067206ff880341fafa621a41d99d63fd81e3e8e

shaders/colorspace: don't apply alpha mode during color encode/decode
This is the wrong place to apply the alpha mode, because it leads to double
applications when the renderer is already changing the alpha mode. It's better
to let the renderer handle this logic, since it's the only place that actually
keeps track of the current state of the image.

pl_color_repr_encode/decode merely _assumed_ that the image was straight alpha
going in.

src/shaders/colorspace.c
@@ -282,9 +282,6 @@ void pl_shader_decode_color(pl_shader sh, struct pl_color_repr *repr,
     GLSL("// pl_shader_decode_color \n"
          "{ \n");
 
-    // Do this first because the following operations are potentially nonlinear
-    pl_shader_set_alpha(sh, repr, PL_ALPHA_INDEPENDENT);
-
     if (repr->sys == PL_COLOR_SYSTEM_XYZ ||
         repr->sys == PL_COLOR_SYSTEM_DOLBYVISION)
     {
@@ -581,9 +578,6 @@ void pl_shader_encode_color(pl_shader sh, const struct pl_color_repr *repr)
             GLSL("color.rgb *= vec3("$"); \n", xyzscale);
         }
     }
-
-    if (repr->alpha == PL_ALPHA_PREMULTIPLIED)
-        GLSL("color.rgb *= vec3(color.a); \n");
 
     GLSL("}\n");
 }

commit 750c344aa902c6279bb904f05be2a99087e4ca25

shaders/colorspace: re-add correct alpha normalization
This was broken in the past, and also not handled in a very elegant way by
the renderer. During the process of fixing the renderer's alpha handling,
this logic was removed in 0067206ff without a replacement. However, that
is also not the correct fix, as the documentation clearly states that the
alpha mode is applied by these functions, and other places in the renderer
depended on this behavior.

Instead, the correct fix to these functions is to re-add the alpha mode
application, but at the correct place in the pipeline, which is on the
RGBA *ingest*, rather than the encoded color. (Basically the opposite place
of what the previous logic was doing)

Fixes: 0067206ff880341fafa621a41d99d63fd81e3e8e
Fixes: https://code.videolan.org/videolan/libplacebo/-/issues/336
Fixes: https://github.com/haasn/libplacebo/issues/332
Fixes: https://github.com/mpv-player/mpv/issues/16396
Closes: https://code.videolan.org/videolan/libplacebo/-/merge_requests/709

src/shaders/colorspace.c
@@ -460,6 +460,7 @@ void pl_shader_decode_color(pl_shader sh, struct pl_color_repr *repr,
         GLSL("color.rgb = pow(max(color.rgb, vec3(0.0)), vec3("$")); \n", gamma);
     }
 
+    pl_shader_set_alpha(sh, repr, PL_ALPHA_INDEPENDENT);
     GLSL("}\n");
 }
 
@@ -471,6 +472,9 @@ void pl_shader_encode_color(pl_shader sh, const struct pl_color_repr *repr)
     sh_describe(sh, "color encoding");
     GLSL("// pl_shader_encode_color \n"
          "{ \n");
+
+    if (repr->alpha == PL_ALPHA_PREMULTIPLIED)
+        GLSL("color.rgb *= vec3(color.a); \n");
 
     switch (repr->sys) {
     case PL_COLOR_SYSTEM_BT_2020_C: