haasn/libplacebo

commit 11b3350fce8d570fea4a374cf265c2ed20aa4a9b

vulkan/command: tie semaphores to queues, not commands
There is no reason for us to have semaphores on a so granular basis - we
only ever need to keep track of commands executed on a given queue, which
will always be executed in submission order already.

Unfortunately, this would break if we end up trying to record two commands
on the same queue at the same time, since the semaphore value would end
up being inconsistent with the submission order. Fortunately, this is never
a case we can currently hit, because all recorded commands are already
locked by the pl_gpu's internal state.

Still, add a safety assertion just to be sure.

src/vulkan/command.c
@@ -64,7 +64,6 @@ static void vk_cmd_destroy(struct vk_cmd *cmd)
     struct vk_ctx *vk = cmd->pool->vk;
     vk_cmd_poll(cmd, UINT64_MAX);
     vk_cmd_reset(cmd);
-    vk->DestroySemaphore(vk->dev, cmd->sync.sem, PL_VK_ALLOC);
     vk->FreeCommandBuffers(vk->dev, cmd->pool->pool, 1, &cmd->buf);
 
     pl_free(cmd);
@@ -84,20 +83,6 @@ static struct vk_cmd *vk_cmd_create(struct vk_cmdpool *pool)
     };
 
     VK(vk->AllocateCommandBuffers(vk->dev, &ainfo, &cmd->buf));
-
-    static const VkSemaphoreTypeCreateInfo stinfo = {
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO,
-        .semaphoreType  = VK_SEMAPHORE_TYPE_TIMELINE,
-        .initialValue   = 0,
-    };
-
-    static const VkSemaphoreCreateInfo sinfo = {
-        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
-        .pNext = &stinfo,
-    };
-
-    VK(vk->CreateSemaphore(vk->dev, &sinfo, PL_VK_ALLOC, &cmd->sync.sem));
-    PL_VK_NAME(SEMAPHORE, cmd->sync.sem, "cmd");
 
     return cmd;
 
@@ -331,11 +316,26 @@ struct vk_cmdpool *vk_cmdpool_create(struct vk_ctx *vk, int qf, int qnum,
         .props      = props,
         .qf         = qf,
         .queues     = pl_calloc(pool, qnum, sizeof(VkQueue)),
+        .sync       = pl_calloc(pool, qnum, sizeof(pl_vulkan_sem)),
         .num_queues = qnum,
     };
 
-    for (int n = 0; n < qnum; n++)
+    static const VkSemaphoreTypeCreateInfo stinfo = {
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_TYPE_CREATE_INFO,
+        .semaphoreType = VK_SEMAPHORE_TYPE_TIMELINE,
+        .initialValue  = 0,
+    };
+
+    static const VkSemaphoreCreateInfo sinfo = {
+        .sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO,
+        .pNext = &stinfo,
+    };
+
+    for (int n = 0; n < qnum; n++) {
         vk->GetDeviceQueue(vk->dev, qf, n, &pool->queues[n]);
+        VK(vk->CreateSemaphore(vk->dev, &sinfo, PL_VK_ALLOC, &pool->sync[n].sem));
+        PL_VK_NAME(SEMAPHORE, pool->sync[n].sem, "cmd");
+    }
 
     VkCommandPoolCreateInfo cinfo = {
         .sType = VK_STRUCTURE_TYPE_COMMAND_POOL_CREATE_INFO,
@@ -362,6 +362,9 @@ void vk_cmdpool_destroy(struct vk_cmdpool *pool)
         vk_cmd_destroy(pool->cmds.elem[i]);
 
     struct vk_ctx *vk = pool->vk;
+    for (int i = 0; i < pool->num_queues; i++)
+        vk->DestroySemaphore(pool->vk->dev, pool->sync[i].sem, PL_VK_ALLOC);
+
     vk->DestroyCommandPool(vk->dev, pool->pool, PL_VK_ALLOC);
     pl_free(pool);
 }
@@ -394,12 +397,10 @@ struct vk_cmd *vk_cmd_begin(struct vk_cmdpool *pool, pl_debug_tag debug_tag)
     };
 
     VK(vk->BeginCommandBuffer(cmd->buf, &binfo));
+    PL_VK_NAME_HANDLE(COMMAND_BUFFER, cmd->buf, PL_DEF(debug_tag, "vk_cmd"));
 
-    debug_tag = PL_DEF(debug_tag, "vk_cmd");
-    PL_VK_NAME_HANDLE(COMMAND_BUFFER, cmd->buf, debug_tag);
-    PL_VK_NAME(SEMAPHORE, cmd->sync.sem, debug_tag);
-
-    cmd->sync.value++;
+    pool->sync[cmd->qindex].value++;
+    cmd->sync = pool->sync[cmd->qindex];
     vk_cmd_sig(cmd, VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT, cmd->sync);
     return cmd;
 
@@ -506,6 +507,7 @@ bool vk_cmd_submit(struct vk_cmd **pcmd)
             PL_TRACE(vk, "    signals %d callbacks", cmd->callbacks.num);
     }
 
+    pl_assert(pool->sync[cmd->qindex].value == cmd->sync.value);
     vk->lock_queue(vk->queue_ctx, pool->qf, cmd->qindex);
     VkResult res = vk_queue_submit2(vk, cmd->queue, &sinfo, VK_NULL_HANDLE);
     vk->unlock_queue(vk->queue_ctx, pool->qf, cmd->qindex);

src/vulkan/command.h
@@ -46,7 +46,7 @@ void vk_dev_callback(struct vk_ctx *vk, vk_cb callback,
 // Thread-safety: Unsafe
 struct vk_cmd {
     struct vk_cmdpool *pool; // pool it was allocated from
-    pl_vulkan_sem sync;      // pending execution, tied to lifetime of device
+    pl_vulkan_sem sync;      // completion value, owned by the cmdpool
     VkQueue queue;           // the submission queue (for recording/pending)
     int qindex;              // the index of `queue` in `pool`
     VkCommandBuffer buf;     // the command buffer itself
@@ -102,6 +102,7 @@ struct vk_cmdpool {
     VkQueueFamilyProperties props;
     int qf; // queue family index
     VkCommandPool pool;
+    pl_vulkan_sem *sync;
     VkQueue *queues;
     int num_queues;
     int idx_queues;

commit 33b5dfada6a84692912e4d41f673f895df79479e

vulkan/command: workaround broken timeline semaphore support on MoltenVK
Commit 11b3350fce8d570fea4a374cf265c2ed20aa4a9b caused synchronization
issues on MoltenVK due to buggy implementation of timeline semaphores on
the driver. Workaround it by signalling a per-command VkFence on
submission and waiting on it instead of the timeline semaphore when on
MoltenVK.

Fixes: https://github.com/mpv-player/mpv/issues/17258

src/vulkan/command.c
@@ -22,6 +22,10 @@
 static VkResult vk_cmd_poll(struct vk_cmd *cmd, uint64_t timeout)
 {
     struct vk_ctx *vk = cmd->pool->vk;
+
+    if (cmd->fence)
+        return vk->WaitForFences(vk->dev, 1, &cmd->fence, VK_TRUE, timeout);
+
     return vk->WaitSemaphores(vk->dev, &(VkSemaphoreWaitInfo) {
         .sType = VK_STRUCTURE_TYPE_SEMAPHORE_WAIT_INFO,
         .semaphoreCount = 1,
@@ -64,6 +68,7 @@ static void vk_cmd_destroy(struct vk_cmd *cmd)
     struct vk_ctx *vk = cmd->pool->vk;
     vk_cmd_poll(cmd, UINT64_MAX);
     vk_cmd_reset(cmd);
+    vk->DestroyFence(vk->dev, cmd->fence, PL_VK_ALLOC);
     vk->FreeCommandBuffers(vk->dev, cmd->pool->pool, 1, &cmd->buf);
 
     pl_free(cmd);
@@ -84,6 +89,17 @@ static struct vk_cmd *vk_cmd_create(struct vk_cmdpool *pool)
 
     VK(vk->AllocateCommandBuffers(vk->dev, &ainfo, &cmd->buf));
 
+    // Fences may be used to workaround buggy timeline semaphore support
+    // on certain platforms. See:
+    // <https://github.com/KhronosGroup/MoltenVK/issues/2697>
+    if (vk->driver_props.driverID == VK_DRIVER_ID_MOLTENVK) {
+        VkFenceCreateInfo finfo = {
+            .sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+            .flags = VK_FENCE_CREATE_SIGNALED_BIT,
+        };
+        VK(vk->CreateFence(vk->dev, &finfo, PL_VK_ALLOC, &cmd->fence));
+        PL_VK_NAME(FENCE, cmd->fence, "cmd");
+    }
     return cmd;
 
 error:
@@ -399,6 +415,9 @@ struct vk_cmd *vk_cmd_begin(struct vk_cmdpool *pool, pl_debug_tag debug_tag)
     VK(vk->BeginCommandBuffer(cmd->buf, &binfo));
     PL_VK_NAME_HANDLE(COMMAND_BUFFER, cmd->buf, PL_DEF(debug_tag, "vk_cmd"));
 
+    if (cmd->fence)
+        vk->ResetFences(vk->dev, 1, &cmd->fence);
+
     pool->sync[cmd->qindex].value++;
     cmd->sync = pool->sync[cmd->qindex];
     vk_cmd_sig(cmd, VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT, cmd->sync);
@@ -509,7 +528,7 @@ bool vk_cmd_submit(struct vk_cmd **pcmd)
 
     pl_assert(pool->sync[cmd->qindex].value == cmd->sync.value);
     vk->lock_queue(vk->queue_ctx, pool->qf, cmd->qindex);
-    VkResult res = vk_queue_submit2(vk, cmd->queue, &sinfo, VK_NULL_HANDLE);
+    VkResult res = vk_queue_submit2(vk, cmd->queue, &sinfo, cmd->fence);
     vk->unlock_queue(vk->queue_ctx, pool->qf, cmd->qindex);
     PL_VK_ASSERT(res, "vkQueueSubmit2");
 

src/vulkan/command.h
@@ -50,6 +50,7 @@ struct vk_cmd {
     VkQueue queue;           // the submission queue (for recording/pending)
     int qindex;              // the index of `queue` in `pool`
     VkCommandBuffer buf;     // the command buffer itself
+    VkFence fence;           // Only used on MoltenVK
     // Command dependencies and signals. Not owned by the vk_cmd.
     PL_ARRAY(VkSemaphoreSubmitInfo) deps;
     PL_ARRAY(VkSemaphoreSubmitInfo) sigs;