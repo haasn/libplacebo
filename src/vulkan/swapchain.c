haasn/libplacebo

commit 1cee493aa9fc083fbdbdd472422248a929e2fbb6

vulkan/swapchain: call sw_resize instead of sw_recreate
This should check if we need to recreate before doing so.

src/vulkan/swapchain.c
@@ -1069,7 +1069,7 @@ static void vk_sw_colorspace_hint(pl_swapchain sw, const struct pl_color_space *
         // also immediately cleanup the retired swapchain. This is needed
         // because the driver might hold Wayland color surface parented to that
         // swapchain.
-        vk_sw_recreate(sw, 0, 0);
+        vk_sw_resize(sw, 0, 0);
         cleanup_retired_swapchains(sw, 0);
     } else {
         set_hdr_metadata(p, &csp->hdr);

commit 54e527552fa74467bcc7692e6985d35540861d19

vulkan/swapchain: fix issues with colorspace_hint PASS_THROUGH path
There were various issues with the previous two commits.

1) Last commit changed the call from vk_sw_recreate to vk_sw_resize but
   the functions don't have the same signature which wasn't accounted
   for.
2) vk_sw_resize locks the mutex, so we needed to unlock it first

Fix these two issues by going back to vk_sw_recreate and checking
p->needs_recreate instead. This effectively reverts 1cee493aa9fc

3) cleanup_retired_swapchains was called with 0 timeout, which meant we
   weren't waiting for swapchains to be destroyed at all. It just so
   happened to work fine in testing, but we should wait until swapchains
   are destroyed.

Fix this by setting UINT64_MAX timeout.

Fixes: 1cee493aa9fc
Fixes: 8a99c8543fd6

src/vulkan/swapchain.c
@@ -1069,8 +1069,10 @@ static void vk_sw_colorspace_hint(pl_swapchain sw, const struct pl_color_space *
         // also immediately cleanup the retired swapchain. This is needed
         // because the driver might hold Wayland color surface parented to that
         // swapchain.
-        vk_sw_resize(sw, 0, 0);
-        cleanup_retired_swapchains(sw, 0);
+        if (p->needs_recreate) {
+            vk_sw_recreate(sw, 0, 0);
+            cleanup_retired_swapchains(sw, UINT64_MAX);
+        }
     } else {
         set_hdr_metadata(p, &csp->hdr);
     }