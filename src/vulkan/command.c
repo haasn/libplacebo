haasn/libplacebo

commit 409c9a822527693dcb5f60c5e37a74a85fae7204

vulkan/context: use `VK_KHR_internally_synchronized_queues`
libavutil deprecated AVVulkanDeviceContext.lock/unlock_queue without
replacement [1]. This prevents libplacebo users (like mpv) from using
queue locking, as those callback will be removed in lavu. To prepare for
that use `VK_KHR_internally_synchronized_queues`.

It's unclear what will happen to devices that do not support
`VK_KHR_internally_synchronized_queues` when the lock/unlock_queue
callbacks are removed.

[1] https://code.ffmpeg.org/FFmpeg/FFmpeg/commit/c102e89448a06d9920920923a4008fee3db4ea37

src/vulkan/context.c
@@ -204,6 +204,10 @@ static const struct vk_ext vk_device_extensions[] = {
         .name = VK_KHR_SWAPCHAIN_MAINTENANCE_1_EXTENSION_NAME,
     }, {
         .name = VK_EXT_SWAPCHAIN_MAINTENANCE_1_EXTENSION_NAME,
+#ifdef VK_KHR_internally_synchronized_queues
+    }, {
+        .name = VK_KHR_INTERNALLY_SYNCHRONIZED_QUEUES_EXTENSION_NAME,
+#endif
     },
 };
 
@@ -235,6 +239,9 @@ const char * const pl_vulkan_recommended_extensions[] = {
     VK_EXT_HOST_IMAGE_COPY_EXTENSION_NAME,
     VK_KHR_SWAPCHAIN_MAINTENANCE_1_EXTENSION_NAME,
     VK_EXT_SWAPCHAIN_MAINTENANCE_1_EXTENSION_NAME,
+#ifdef VK_KHR_internally_synchronized_queues
+    VK_KHR_INTERNALLY_SYNCHRONIZED_QUEUES_EXTENSION_NAME,
+#endif
 };
 
 const int pl_vulkan_num_recommended_extensions =
@@ -247,8 +254,18 @@ static_assert(PL_ARRAY_SIZE(pl_vulkan_recommended_extensions) + 1 ==
               "vk_device_extensions?");
 
 // Recommended features; keep in sync with libavutil vulkan hwcontext
+#ifdef VK_KHR_internally_synchronized_queues
+static const VkPhysicalDeviceInternallySynchronizedQueuesFeaturesKHR synchronized_queues = {
+    .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_INTERNALLY_SYNCHRONIZED_QUEUES_FEATURES_KHR,
+    .internallySynchronizedQueues = true,
+};
+#endif
+
 static const VkPhysicalDeviceSwapchainMaintenance1FeaturesKHR swapchain_maintenance1 = {
     .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_SWAPCHAIN_MAINTENANCE_1_FEATURES_KHR,
+#ifdef VK_KHR_internally_synchronized_queues
+    .pNext = (void *) &synchronized_queues,
+#endif
     .swapchainMaintenance1 = true,
 };
 
@@ -1085,6 +1102,8 @@ error:
     return dev;
 }
 
+static void queue_lock_noop(void *priv, uint32_t qf, uint32_t qidx) {}
+
 static void lock_queue_internal(void *priv, uint32_t qf, uint32_t qidx)
 {
     struct vk_ctx *vk = priv;
@@ -1097,9 +1116,25 @@ static void unlock_queue_internal(void *priv, uint32_t qf, uint32_t qidx)
     pl_mutex_unlock(&vk->queue_locks.elem[qf].elem[qidx]);
 }
 
+static bool has_synchronized_queues(const VkPhysicalDeviceFeatures2 *features)
+{
+#ifdef VK_KHR_internally_synchronized_queues
+    const VkPhysicalDeviceInternallySynchronizedQueuesFeaturesKHR *isq = vk_find_struct(features,
+            VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_INTERNALLY_SYNCHRONIZED_QUEUES_FEATURES_KHR);
+    return isq && isq->internallySynchronizedQueues;
+#else
+    return false;
+#endif
+}
+
 static void init_queue_locks(struct vk_ctx *vk, uint32_t qfnum,
                              const VkQueueFamilyProperties *qfs)
 {
+    if (has_synchronized_queues(&vk->features)) {
+        vk->lock_queue = queue_lock_noop;
+        vk->unlock_queue = queue_lock_noop;
+        return;
+    }
     vk->queue_locks.elem = pl_calloc_ptr(vk->alloc, qfnum, vk->queue_locks.elem);
     vk->queue_locks.num = qfnum;
     for (int i = 0; i < qfnum; i++) {
@@ -1169,7 +1204,6 @@ static bool device_init(struct vk_ctx *vk, const struct pl_vulkan_params *params
     vk->GetPhysicalDeviceQueueFamilyProperties(vk->physd, &qfnum, NULL);
     VkQueueFamilyProperties *qfs = pl_calloc_ptr(tmp, qfnum, qfs);
     vk->GetPhysicalDeviceQueueFamilyProperties(vk->physd, &qfnum, qfs);
-    init_queue_locks(vk, qfnum, qfs);
 
     PL_DEBUG(vk, "Queue families supported by device:");
     for (int i = 0; i < qfnum; i++) {
@@ -1313,8 +1347,14 @@ static bool device_init(struct vk_ctx *vk, const struct pl_vulkan_params *params
         use_qf |= qfs[i].queueFlags & params->extra_queues;
         if (!use_qf)
             continue;
+        VkDeviceQueueCreateFlags qflags = 0;
+#ifdef VK_KHR_internally_synchronized_queues
+        if (has_synchronized_queues(&vk->features))
+            qflags |= VK_DEVICE_QUEUE_CREATE_INTERNALLY_SYNCHRONIZED_BIT_KHR;
+#endif
         PL_ARRAY_APPEND(tmp, qinfos, (VkDeviceQueueCreateInfo) {
             .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
+            .flags = qflags,
             .queueFamilyIndex = i,
             .queueCount = qfs[i].queueCount,
             .pQueuePriorities = pl_calloc(tmp, qfs[i].queueCount, sizeof(float)),
@@ -1329,6 +1369,8 @@ static bool device_init(struct vk_ctx *vk, const struct pl_vulkan_params *params
         .ppEnabledExtensionNames = vk->exts.elem,
         .enabledExtensionCount = vk->exts.num,
     };
+
+    init_queue_locks(vk, qfnum, qfs);
 
     PL_INFO(vk, "Creating vulkan device%s", vk->exts.num ? " with extensions:" : "");
     for (int i = 0; i < vk->exts.num; i++)

src/include/libplacebo/vulkan.h
@@ -433,6 +433,9 @@ struct pl_vulkan_import_params {
 
     // Functions for locking a queue. If set, these will be used instead of
     // libplacebo's internal functions for `pl_vulkan.(un)lock_queue`.
+    // If not set, libplacebo will use its own internal locking, unless the
+    // device was created with VK_KHR_internally_synchronized_queues enabled
+    // (as indicated by `features`), in which case locking is skipped.
     void (*lock_queue)(void *ctx, uint32_t qf, uint32_t qidx);
     void (*unlock_queue)(void *ctx, uint32_t qf, uint32_t qidx);
     void *queue_ctx;

commit 0d1bded143126b70304c7f7f3822faab8bc81312

vulkan/command: replace vkGetDeviceQueue with vkGetDeviceQueue2
vkGetDeviceQueue2 with flags = 0 is equivalent to vkGetDeviceQueue and
is available since Vulkan 1.1. Needed to support queues created with
non-zero VkDeviceQueueCreateFlags.

Fixes VUID-vkGetDeviceQueue-flags-01841 VVL error.
Fixes: 409c9a822527 ("vulkan/context: use `VK_KHR_internally_synchronized_queues`")

src/vulkan/command.c
@@ -348,7 +348,13 @@ struct vk_cmdpool *vk_cmdpool_create(struct vk_ctx *vk, int qf, int qnum,
     };
 
     for (int n = 0; n < qnum; n++) {
-        vk->GetDeviceQueue(vk->dev, qf, n, &pool->queues[n]);
+        VkDeviceQueueInfo2 qinfo = {
+            .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_INFO_2,
+            .flags = vk->queue_flags,
+            .queueFamilyIndex = qf,
+            .queueIndex       = n,
+        };
+        vk->GetDeviceQueue2(vk->dev, &qinfo, &pool->queues[n]);
         VK(vk->CreateSemaphore(vk->dev, &sinfo, PL_VK_ALLOC, &pool->sync[n].sem));
         PL_VK_NAME(SEMAPHORE, pool->sync[n].sem, "cmd");
     }

src/vulkan/common.h
@@ -94,6 +94,7 @@ struct vk_ctx {
     void (*lock_queue)(void *queue_ctx, uint32_t qf, uint32_t idx);
     void (*unlock_queue)(void *queue_ctx, uint32_t qf, uint32_t idx);
     void *queue_ctx;
+    VkDeviceQueueCreateFlags queue_flags;
 
     // Pending commands. These are shared for the entire mpvk_ctx to ensure
     // submission and callbacks are FIFO
@@ -207,7 +208,7 @@ struct vk_ctx {
     PL_VK_FUN(FreeCommandBuffers);
     PL_VK_FUN(FreeMemory);
     PL_VK_FUN(GetBufferMemoryRequirements);
-    PL_VK_FUN(GetDeviceQueue);
+    PL_VK_FUN(GetDeviceQueue2);
     PL_VK_FUN(GetImageDrmFormatModifierPropertiesEXT);
     PL_VK_FUN(GetImageMemoryRequirements2);
     PL_VK_FUN(GetImageSubresourceLayout);

src/vulkan/context.c
@@ -435,7 +435,7 @@ static const struct vk_fun vk_dev_funs[] = {
     PL_VK_DEV_FUN(FreeCommandBuffers),
     PL_VK_DEV_FUN(FreeMemory),
     PL_VK_DEV_FUN(GetBufferMemoryRequirements),
-    PL_VK_DEV_FUN(GetDeviceQueue),
+    PL_VK_DEV_FUN(GetDeviceQueue2),
     PL_VK_DEV_FUN(GetImageMemoryRequirements2),
     PL_VK_DEV_FUN(GetImageSubresourceLayout),
     PL_VK_DEV_FUN(GetPipelineCacheData),
@@ -1347,14 +1347,13 @@ static bool device_init(struct vk_ctx *vk, const struct pl_vulkan_params *params
         use_qf |= qfs[i].queueFlags & params->extra_queues;
         if (!use_qf)
             continue;
-        VkDeviceQueueCreateFlags qflags = 0;
 #ifdef VK_KHR_internally_synchronized_queues
         if (has_synchronized_queues(&vk->features))
-            qflags |= VK_DEVICE_QUEUE_CREATE_INTERNALLY_SYNCHRONIZED_BIT_KHR;
+            vk->queue_flags |= VK_DEVICE_QUEUE_CREATE_INTERNALLY_SYNCHRONIZED_BIT_KHR;
 #endif
         PL_ARRAY_APPEND(tmp, qinfos, (VkDeviceQueueCreateInfo) {
             .sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO,
-            .flags = qflags,
+            .flags = vk->queue_flags,
             .queueFamilyIndex = i,
             .queueCount = qfs[i].queueCount,
             .pQueuePriorities = pl_calloc(tmp, qfs[i].queueCount, sizeof(float)),