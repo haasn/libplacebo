haasn/libplacebo

commit 464d6eab8900c87a539943d0cd8575fffe566f72

vulkan/command: fix invalid access mask on pure layout transition
The MEMORY_READ_BIT added by 527fe3fd was and'd away by the
vk_access_write at the end of the function, leaving only
MEMORY_WRITE_BIT in the access mask which is illegal for images in read
only layouts.

Drop the access assignment entirely, ALL_COMMANDS_BIT alone is sufficient
to produce a non-empty synchronization scope.

Fixes VVL warning with vulkan hwdec.

src/vulkan/command.c
@@ -278,7 +278,6 @@ struct vk_sync_scope vk_sem_barrier(struct vk_cmd *cmd, struct vk_sem *sem,
         // synchronization scope or else the layers complain
         if (stage == VK_PIPELINE_STAGE_2_NONE) {
             last.stage = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
-            last.access = VK_ACCESS_2_MEMORY_READ_BIT | VK_ACCESS_2_MEMORY_WRITE_BIT;
         }
     }
 

commit d4624cbfb37fdef337a7da5794b66202671a89cd

vulkan/command: fix write-after-write hazard on layout transition
464d6eab8900 assumed that producing a non-empty synchronization scope
was sufficient for satisfying _all_ warnings, but that's clearly not the
case. VVL complains about write-after-write in a scenario I neglected to
test. When no overlays are present and no hardware decoding is being
done, i.e. `mpv --no-config --hwdec=no --sub-visibility=no --osc=no`.

Fix it by reintroducing the WRITE_BIT but only for write transitions.
464d6eab8900 is still correct about READ_BIT being a no-op, so no need
to reintroduce that one.

Fixes: 464d6eab8900 ("vulkan/command: fix invalid access mask on pure layout transition")

src/vulkan/command.c
@@ -276,8 +276,9 @@ struct vk_sync_scope vk_sem_barrier(struct vk_cmd *cmd, struct vk_sem *sem,
         // Special case: this is a pure layout transition (with no command),
         // in this case we need to ensure that we still emit some sort of
         // synchronization scope or else the layers complain
-        if (stage == VK_PIPELINE_STAGE_2_NONE) {
+        if (stage == VK_PIPELINE_STAGE_2_NONE && is_write) {
             last.stage = VK_PIPELINE_STAGE_2_ALL_COMMANDS_BIT;
+            last.access = VK_ACCESS_2_MEMORY_WRITE_BIT;
         }
     }
 