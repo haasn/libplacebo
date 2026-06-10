haasn/libplacebo

commit 0758cbe03ace108c73628cdd07095fa26a465c63

Revert "renderer: never use linear downscaling on HDR sources"
This reverts commit 030b59d1771cdfe7c563480aa187dbc11645ca36.

Since we have antiringing support for EWA scalers now, we have no need to
not use linear scaling for HDR content.

Closes: https://github.com/haasn/libplacebo/issues/281

Signed-off-by: Shengyu Qu <wiagn@4d2.org>

src/renderer.c
@@ -2031,10 +2031,12 @@ static bool pass_scale_main(struct pass_state *pass)
     if (params->disable_linear_scaling || fbofmt->component_depth[0] < 16)
         use_sigmoid = use_linear = false;
 
-    // Avoid sigmoidization for HDR content because it clips to [0,1], and
-    // linearization because it causes very nasty ringing artefacts.
-    if (pl_color_space_is_hdr(&img->color))
-        use_sigmoid = use_linear = false;
+    // Avoid sigmoidization for HDR content because it clips to [0,1]
+    if (pl_color_space_is_hdr(&img->color)) {
+        use_sigmoid = false;
+        if (fbofmt->type != PL_FMT_FLOAT)
+            use_linear = false; // linear HDR needs out-of-range signals
+    }
 
     if (!(use_linear || use_sigmoid) && img->color.transfer == PL_COLOR_TRC_LINEAR) {
         img->color.transfer = image->color.transfer;

commit 2d0979fb54e025e904c7372666fffbf5dae40f66

renderer: use pass->image for max_peak in hdr_update_peak()
With commit 0758cbe0, pl_color_transfer_nominal_peak() inside
hdr_update_peak() would return 1.0 when linear downscaling is applied to HDR
content instead of PQ/HLG's actual maximum nominal peak. Use pass->image than
pass->img to solve this.

Closes: https://github.com/haasn/libplacebo/issues/371

Fixes: 0758cbe0 ("Revert "renderer: never use linear downscaling on HDR sources"")
Signed-off-by: Shengyu Qu <wiagn@4d2.org>

src/renderer.c
@@ -1184,7 +1184,10 @@ static void hdr_update_peak(struct pass_state *pass)
 {
     const struct pl_render_params *params = pass->params;
     pl_renderer rr = pass->rr;
-    if (!params->peak_detect_params || !pl_color_space_is_hdr(&pass->img.color))
+
+    // Use pass->image than pass->img to deal with cases that input is HDR
+    // content but already linearized when scaling.
+    if (!params->peak_detect_params || !pl_color_space_is_hdr(&pass->image.color))
         goto cleanup;
 
     if (rr->errors & PL_RENDER_ERR_PEAK_DETECT)
@@ -1196,9 +1199,9 @@ static void hdr_update_peak(struct pass_state *pass)
     if (!rr->gpu->limits.max_ssbo_size)
         goto cleanup;
 
-    float max_peak = pl_color_transfer_nominal_peak(pass->img.color.transfer) *
+    float max_peak = pl_color_transfer_nominal_peak(pass->image.color.transfer) *
                      PL_COLOR_SDR_WHITE;
-    if (pass->img.color.transfer == PL_COLOR_TRC_HLG)
+    if (pass->image.color.transfer == PL_COLOR_TRC_HLG)
         max_peak = pass->img.color.hdr.max_luma;
     if (max_peak <= pass->target.color.hdr.max_luma + 1e-6)
         goto cleanup; // no adaptation needed